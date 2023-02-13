d657f3ea387f8095127d18271a42568aca6dec0a
Michiel de Jong <michiel@unhosted.org>
2023-02-13 09:00:53 +0000

    More docker pull

--- scripts/init-sciencemesh.sh
+++ scripts/init-sciencemesh.sh
@@ -6,3 +6,5 @@ docker pull pondersource/dev-stock-nc2-sciencemesh
 docker pull pondersource/dev-stock-oc1-sciencemesh
 docker pull pondersource/dev-stock-oc2-sciencemesh
 docker pull pondersource/dev-stock-ocmstub
+docker pull jlesage/firefox:v1.17.1
+docker pull mariadb
