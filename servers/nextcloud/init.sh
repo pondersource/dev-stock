50edb6164aff886dadac8dd132ae06aa947dd139
Michiel de Jong <michiel@unhosted.org>
2023-02-14 13:53:56 +0000

    Call all databases efss

--- servers/nextcloud/init.sh
+++ servers/nextcloud/init.sh
@@ -1,4 +1,4 @@
-php console.php maintenance:install --admin-user $USER --admin-pass $PASS --database "mysql" --database-name "nextcloud" --database-user "root" --database-pass "eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek" --database-host "$DBHOST"
+php console.php maintenance:install --admin-user $USER --admin-pass $PASS --database "mysql" --database-name "efss" --database-user "root" --database-pass "eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek" --database-host "$DBHOST"
 php console.php app:disable firstrunwizard
 php console.php app:enable sciencemesh
 sed -i "8 i\      1 => 'nc1.docker'," /var/www/html/config/config.php
