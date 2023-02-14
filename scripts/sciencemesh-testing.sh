f753dea923d108a17f73c589fa96c62dad2415e4
Michiel de Jong <michiel@unhosted.org>
2023-02-14 16:05:57 +0000

    Corrections

--- scripts/sciencemesh-testing.sh
+++ scripts/sciencemesh-testing.sh
@@ -15,32 +15,32 @@ function waitForPort {
   echo $1 port $2 is open
 }
 
-docker run --restart=always -d --network=testnet --name=reva{$EFSS1}1.docker -e HOST=reva{$EFSS1}1 pondersource/dev-stock-revad
+docker run --restart=always -d --network=testnet --name=reva${EFSS1}1.docker -e HOST=reva${EFSS1}1 pondersource/dev-stock-revad
 docker run --restart=always -d --network=testnet --name=maria1.docker -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
-docker run --restart=always -d --network=testnet --name={$EFSS1}1.docker -v $REPO_ROOT/$EFSS1-sciencemesh:/var/www/html/apps/$EFSS1-sciencemesh  pondersource/dev-stock-{$EFSS1}1-sciencemesh
-docker run --restart=always -d --network=testnet --name=reva{$EFSS2}2.docker -e HOST=reva{$EFSS2}2 pondersource/dev-stock-revad
+docker run --restart=always -d --network=testnet --name=${EFSS1}1.docker -v $REPO_ROOT/$EFSS1-sciencemesh:/var/www/html/apps/$EFSS1-sciencemesh  pondersource/dev-stock-${EFSS1}1-sciencemesh
+docker run --restart=always -d --network=testnet --name=reva${EFSS2}2.docker -e HOST=reva${EFSS2}2 pondersource/dev-stock-revad
 docker run --restart=always -d --network=testnet --name=maria2.docker -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
-docker run --restart=always -d --network=testnet --name={$EFSS2}2.docker -v $REPO_ROOT/$EFSS2-sciencemesh:/var/www/html/apps/$EFSS2-sciencemesh pondersource/dev-stock-{$EFSS2}2-sciencemesh
+docker run --restart=always -d --network=testnet --name=${EFSS2}2.docker -v $REPO_ROOT/$EFSS2-sciencemesh:/var/www/html/apps/$EFSS2-sciencemesh pondersource/dev-stock-${EFSS2}2-sciencemesh
 docker run --restart=always -d --network=testnet --name=meshdir.docker pondersource/dev-stock-ocmstub
 docker run --restart=always -d --network=testnet --name=rclone.docker rclone/rclone rcd -vv --rc-user=rcloneuser --rc-pass=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --rc-addr=0.0.0.0:5572 --server-side-across-configs=true --log-file=/dev/stdout
 docker run --restart=always -d --name=firefox -p 5800:5800 -v /tmp/shm:/config:rw --network=testnet --shm-size 2g jlesage/firefox:v1.17.1
 
 waitForPort maria1.docker 3306
-waitForPort {$EFSS1}1.docker 443
-docker exec -e DBHOST=maria1.docker -e USER=einstein -e PASS=relativity  -u www-data {$EFSS1}1.docker sh /init.sh
-docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://reva{$EFSS1}1.docker/');"
+waitForPort ${EFSS1}1.docker 443
+docker exec -e DBHOST=maria1.docker -e USER=einstein -e PASS=relativity  -u www-data ${EFSS1}1.docker sh /init.sh
+docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://reva${EFSS1}1.docker/');"
 docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'revaSharedSecret', 'shared-secret-1');"
 docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'meshDirectoryUrl', 'https://meshdir.docker/meshdir');"
 
 waitForPort maria2.docker 3306
-waitForPort {$EFSS2}2.docker 443
-docker exec -e DBHOST=maria2.docker -e USER=marie -e PASS=radioactivity -u www-data {$EFSS2}2.docker sh /init.sh
-docker exec maria2.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://reva{$EFSS2}2.docker/');"
+waitForPort ${EFSS2}2.docker 443
+docker exec -e DBHOST=maria2.docker -e USER=marie -e PASS=radioactivity -u www-data ${EFSS2}2.docker sh /init.sh
+docker exec maria2.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://reva${EFSS2}2.docker/');"
 docker exec maria2.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'revaSharedSecret', 'shared-secret-2');"
 docker exec maria2.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'meshDirectoryUrl', 'https://meshdir.docker/meshdir');"
 
-echo Now browse to http://ocmhost:5800 and inside there to https://{$EFSS1}1.docker
+echo Now browse to http://ocmhost:5800 and inside there to https://${EFSS1}1.docker
 echo Log in as einstein / relativity
 echo Go to the ScienceMesh app and generate a token
-echo Click it to go to the meshdir server, and choose {$EFSS2}2 there.
-echo Log in on https://{$EFSS2}2.docker as marie / radioactivity
+echo Click it to go to the meshdir server, and choose ${EFSS2}2 there.
+echo Log in on https://${EFSS2}2.docker as marie / radioactivity
