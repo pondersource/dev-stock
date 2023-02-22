6c833d542a6aedeca366cfa0f2dee5b64a29c621
Michiel de Jong <michiel@unhosted.org>
2023-02-22 12:41:11 +0000

    relative reva path

--- scripts/datatx-testing.sh
+++ scripts/datatx-testing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker run -d --network=testnet --name=revad1.docker -v /workspace/reva:/reva -e HOST=revad1 pondersource/dev-stock-revad
+docker run -d --network=testnet --name=revad1.docker -v `pwd`/../reva:/reva -e HOST=revad1 pondersource/dev-stock-revad
 docker run -d --network=testnet --name=rclone1.docker rclone/rclone rcd -vv --rc-user=rcloneuser --rc-pass=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --rc-addr=0.0.0.0:5572 --server-side-across-configs=true --log-file=/dev/stdout
-docker run -d --network=testnet --name=revad2.docker -v /workspace/reva:/reva -e HOST=revad2 pondersource/dev-stock-revad
+docker run -d --network=testnet --name=revad2.docker -v `pwd`/../reva:/reva -e HOST=revad2 pondersource/dev-stock-revad
 docker run -d --network=testnet --name=rclone2.docker rclone/rclone rcd -vv --rc-user=rcloneuser --rc-pass=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --rc-addr=0.0.0.0:5572 --server-side-across-configs=true --log-file=/dev/stdout
