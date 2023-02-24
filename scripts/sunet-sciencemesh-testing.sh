0907eee28f69a6dc159b39591041b11724b6d24a
Michiel de Jong <michiel@unhosted.org>
2023-02-24 10:28:48 +0000

    compare side-by-side

--- scripts/sunet-sciencemesh-testing.sh
+++ scripts/sunet-sciencemesh-testing.sh
@@ -11,11 +11,12 @@ export EFSS2=nc
 # git clone https://github.com/SUNET/nextcloud-custom
 # cd nextcloud-custom
 # git checkout b9097abdcf6757e34f19442b28de6e4d8d0637e8
-# or to compare: git checkout db6fbc2d56b44f406e3
-# docker build -t sunet-sciencemesh .
+# docker build -t sunet-sciencemesh-b9097abdcf6757e34f19442 .
+# git checkout db6fbc2d56b44f406e3
+# docker build -t sunet-sciencemesh-db6fbc2d56b44f406e3 .
 # cd ../dev-stock
 # ./scripts/clean.sh
-# ./scripts/sunet-sciencemesh-testing.sh
+# ./scripts/sunet-sciencemesh-testing.shc
 
 export REPO_ROOT=`pwd`
 [ ! -d "./scripts" ] && echo "Directory ./scripts DOES NOT exist inside $REPO_ROOT, are you running this from the repo root?" && exit 1
@@ -33,11 +34,11 @@ function waitForPort {
 
 docker run -d --network=testnet --name=reva${EFSS1}1.docker -e HOST=reva${EFSS1}1 pondersource/dev-stock-revad
 docker run -d --network=testnet --name=maria1.docker -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
-docker run -d --network=testnet --name=${EFSS1}1.docker -v $REPO_ROOT/$EFSS1-sciencemesh:/var/www/html/apps/sciencemesh sunet-sciencemesh /usr/sbin/apache2ctl -DFOREGROUND
+docker run -d --network=testnet --name=${EFSS1}1.docker -v $REPO_ROOT/$EFSS1-sciencemesh:/var/www/html/apps/sciencemesh sunet-sciencemesh-db6fbc2d56b44f406e3 /usr/sbin/apache2ctl -DFOREGROUND
 
 docker run -d --network=testnet --name=reva${EFSS2}2.docker -e HOST=reva${EFSS2}2 pondersource/dev-stock-revad
 docker run -d --network=testnet --name=maria2.docker -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
-docker run -d --network=testnet --name=${EFSS2}2.docker -v $REPO_ROOT/$EFSS2-sciencemesh:/var/www/html/apps/sciencemesh sunet-sciencemesh /usr/sbin/apache2ctl -DFOREGROUND
+docker run -d --network=testnet --name=${EFSS2}2.docker -v $REPO_ROOT/$EFSS2-sciencemesh:/var/www/html/apps/sciencemesh sunet-sciencemesh-b9097abdcf6757e34f19442 /usr/sbin/apache2ctl -DFOREGROUND
 docker run -d --network=testnet --name=meshdir.docker pondersource/dev-stock-ocmstub
 docker run -d --network=testnet --name=rclone.docker rclone/rclone rcd -vv --rc-user=rcloneuser --rc-pass=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --rc-addr=0.0.0.0:5572 --server-side-across-configs=true --log-file=/dev/stdout
 docker run -d --name=firefox -p 5800:5800 -v /tmp/shm:/config:rw --network=testnet --shm-size 2g jlesage/firefox:v1.17.1
