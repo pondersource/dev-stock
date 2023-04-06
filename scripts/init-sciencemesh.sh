f9d52f78f78ec10c4d00e1904cd3f1b302eeb90d
Michiel de Jong <michiel@unhosted.org>
2023-04-06 11:38:57 +0200

    Update init-sciencemesh.sh

--- scripts/init-sciencemesh.sh
+++ scripts/init-sciencemesh.sh
@@ -9,6 +9,6 @@ docker pull pondersource/dev-stock-ocmstub
 docker pull jlesage/firefox:v1.17.1
 docker pull mariadb
 # docker pull rclone/rclone
-[ ! -d "oc-sciencemesh" ] && git clone --branch=oc-10 https://github.com/pondersource/nc-sciencemesh oc-sciencemesh && docker run -it -v "$(pwd)/oc-sciencemesh:/var/www/html/apps/sciencemesh"  --workdir /var/www/html/apps/sciencemesh pondersource/dev-stock-oc1-sciencemesh make composer
+[ ! -d "oc-sciencemesh" ] && git clone --branch=oc-10-take-2 https://github.com/pondersource/nc-sciencemesh oc-sciencemesh && docker run -it -v "$(pwd)/oc-sciencemesh:/var/www/html/apps/sciencemesh"  --workdir /var/www/html/apps/sciencemesh pondersource/dev-stock-oc1-sciencemesh make composer
 [ ! -d "nc-sciencemesh" ] && git clone https://github.com/pondersource/nc-sciencemesh && docker run -it -v "$(pwd)/nc-sciencemesh:/var/www/html/apps/sciencemesh"  --workdir /var/www/html/apps/sciencemesh pondersource/dev-stock-nc1-sciencemesh make composer
 docker network inspect testnet >/dev/null 2>&1 || docker network create testnet
