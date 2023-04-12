7e18e4b45cbca2a95f3e9546302c9157a4246681
Michiel de Jong <michiel@unhosted.org>
2023-04-12 14:34:54 +0000

    Fix db scripts

--- scripts/maria1.sh
+++ scripts/maria1.sh
@@ -1,1 +1,1 @@
-docker exec -it maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek nextcloud 
+docker exec -it maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss
