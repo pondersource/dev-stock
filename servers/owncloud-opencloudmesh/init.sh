23760f59f04bb582d708aab20e862f4f3eb85f84
Michiel de Jong <michiel@unhosted.org>
2023-04-12 17:31:43 +0000

    Configure ocmController and groupExternalManager

--- servers/owncloud-opencloudmesh/init.sh
+++ servers/owncloud-opencloudmesh/init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 php console.php maintenance:install --admin-user $USER --admin-pass $PASS --database "mysql" --database-name "efss" --database-user "root" --database-pass "eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek" --database-host "$DBHOST"
 php console.php app:disable firstrunwizard
 sed -i "8 i\      1 => 'oc1.docker'," /var/www/html/config/config.php
@@ -11,3 +13,5 @@ php console.php app:enable opencloudmesh
 echo Editing Config
 sed -i "3 i\  'sharing.managerFactory' => 'OCA\\\\OpenCloudMesh\\\\ShareProviderFactory'," /var/www/html/config/config.php
 sed -i "4 i\  'sharing.remoteShareesSearch' => 'OCA\\\\OpenCloudMesh\\\\ShareeSearchPlugin'," /var/www/html/config/config.php
+sed -i "5 i\  'sharing.ocmController' => 'OCA\\\\OpenCloudMesh\\\\OcmController'," /var/www/html/config/config.php
+sed -i "6 i\  'sharing.groupExternalManager' => 'OCA\\\\OpenCloudMesh\\\\GroupExternalManager'," /var/www/html/config/config.php
