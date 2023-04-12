e6f302ac87019bf1c742606584bb611cb67cb40e
Michiel de Jong <michiel@unhosted.org>
2023-04-12 14:32:29 +0000

    Fix docker mounts

--- scripts/oc-opencloudmesh-testing.sh
+++ scripts/oc-opencloudmesh-testing.sh
@@ -24,7 +24,6 @@ echo "starting maria1.docker"
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria1.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 echo "starting oc1.docker"
 docker run -d --network=testnet --name=oc1.docker \
-  -v $REPO_DIR/rd-sram-integration:/var/www/html/apps/rd-sram-integration \
   -v $REPO_DIR/oc-opencloudmesh:/var/www/html/apps/oc-opencloudmesh \
   -v $REPO_DIR/core/apps/files_sharing:/var/www/html/apps/files_sharing \
   -v $REPO_DIR/core/apps/federatedfilesharing:/var/www/html/apps/federatedfilesharing \
@@ -34,8 +33,9 @@ echo "starting maria2.docker"
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria2.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 echo "starting oc2.docker"
 docker run -d --network=testnet --name=oc2.docker \
-  -v $REPO_DIR:/var/www/html/apps/rd-sram-integration \
+  -v $REPO_DIR/oc-opencloudmesh:/var/www/html/apps/oc-opencloudmesh \
   -v $REPO_DIR/core/apps/files_sharing:/var/www/html/apps/files_sharing \
+  -v $REPO_DIR/core/apps/federatedfilesharing:/var/www/html/apps/federatedfilesharing \
   pondersource/dev-stock-oc2-opencloudmesh
 
 echo "starting firefox tester"
