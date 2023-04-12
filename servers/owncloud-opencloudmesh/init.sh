1098e287b1d22ecc60eb9b7cbecf190f6c57bc2c
Michiel de Jong <michiel@unhosted.org>
2023-04-12 17:49:31 +0000

    use quotes

--- servers/owncloud-opencloudmesh/init.sh
+++ servers/owncloud-opencloudmesh/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-php console.php maintenance:install --admin-user $USER --admin-pass $PASS --database "mysql" --database-name "efss" --database-user "root" --database-pass "eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek" --database-host "$DBHOST"
+php console.php maintenance:install --admin-user "$USER" --admin-pass "$PASS" --database "mysql" --database-name "efss" --database-user "root" --database-pass "eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek" --database-host "$DBHOST"
 php console.php app:disable firstrunwizard
 sed -i "8 i\      1 => 'oc1.docker'," /var/www/html/config/config.php
 sed -i "9 i\      2 => 'oc2.docker'," /var/www/html/config/config.php
