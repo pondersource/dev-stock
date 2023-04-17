c2fd1a8204636fdd1a193b8deb74c3ec3abecb59
Michiel de Jong <michiel@unhosted.org>
2023-04-17 13:57:42 +0200

    Check out dev branch of oc-opencloudmesh, fix #7

--- scripts/init-oc-opencloudmesh.sh
+++ scripts/init-oc-opencloudmesh.sh
@@ -5,4 +5,4 @@ docker pull pondersource/dev-stock-oc2-opencloudmesh
 docker pull jlesage/firefox:v1.17.1
 docker pull mariadb
 [ ! -d "core" ] && git clone --depth=1 --branch=accept-ocm-to-groups https://github.com/pondersource/core
-[ ! -d "oc-opencloudmesh" ] && git clone https://github.com/pondersource/oc-opencloudmesh
+[ ! -d "oc-opencloudmesh" ] && git clone --branch=dev https://github.com/pondersource/oc-opencloudmesh
