9f8110171e4110a7bfc3118b99eca43466df17ad
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-04-28 09:52:08 +0000

    modify: use --force flag on link command

--- scripts/switch-php.sh
+++ scripts/switch-php.sh
@@ -6,8 +6,7 @@ FILE="/usr/bin/php${1}"
 
 if [[ -n "${1}" ]]; then
     if [[ -f "${FILE}" ]]; then
-        sudo rm --force /usr/bin/php.default
-        sudo ln --symbolic "${FILE}" /usr/bin/php.default
+        sudo ln --symbolic --force "${FILE}" /usr/bin/php.default
     else
         echo "This version is not available in this system."
     fi
