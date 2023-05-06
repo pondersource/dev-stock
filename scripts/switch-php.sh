01eeb7a5affdcc3eb162d05ff13aba1a631b50fb
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-05-06 08:14:57 +0000

    modify: use update-alternatives

--- scripts/switch-php.sh
+++ scripts/switch-php.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
+# exit immediately if a command exits with a non-zero status.
 set -e
 
 FILE="/usr/bin/php${1}"
 
 if [[ -n "${1}" ]]; then
     if [[ -f "${FILE}" ]]; then
-        sudo ln --symbolic --force "${FILE}" /usr/bin/php.default
+        update-alternatives --set php           "/usr/bin/php${1}"
+        update-alternatives --set phar          "/usr/bin/phar${1}"
+        update-alternatives --set phar.phar     "/usr/bin/phar.phar${1}"
     else
         echo "This version is not available in this system."
     fi
 else
    echo "You didn't provide any version number!"
 fi
-
-
