afc64fcd123d6d04ab403bac88cdfa19f56097ef
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-05-12 12:46:15 +0000

    update: use quotes for variables

--- docker/gencerts.sh
+++ docker/gencerts.sh
@@ -3,20 +3,20 @@
 set -e
 
 function createCert {
-  echo Generating key and CSR for $1.docker
+  echo "Generating key and CSR for ${1}.docker"
   openssl req -new -nodes \
-    -out ../tls/$1.csr \
-    -keyout ../tls/$1.key \
-    -subj "/C=RO/ST=Bucharest/L=Bucharest/O=IT/CN=$1.docker"
+    -out "../tls/${1}.csr" \
+    -keyout "../tls/${1}.key" \
+    -subj "/C=RO/ST=Bucharest/L=Bucharest/O=IT/CN=${1}.docker"
   echo Creating extfile
-  echo "subjectAltName = @alt_names" > ../tls/$1.cnf
-  echo "[alt_names]" >> ../tls/$1.cnf
-  echo "DNS.1 = $1.docker" >> ../tls/$1.cnf
+  echo "subjectAltName = @alt_names" > "../tls/${1}.cnf"
+  echo "[alt_names]" >> "../tls/${1}.cnf"
+  echo "DNS.1 = ${1}.docker" >> "../tls/${1}.cnf"
 
-  echo Signing CSR for $1.docker, creating cert.
-  openssl x509 -req -days 3650 -in ../tls/$1.csr \
+  echo "Signing CSR for ${1}.docker, creating cert."
+  openssl x509 -req -days 3650 -in "../tls/${1}.csr" \
     -CA ../tls/ocm-ca.crt -CAkey ../tls/ocm-ca.key -CAcreateserial \
-    -out ../tls/$1.crt -extfile ../tls/$1.cnf
+    -out "../tls/${1}.crt" -extfile "../tls/${1}.cnf"
 }
 
 rm --recursive --force ../tls
