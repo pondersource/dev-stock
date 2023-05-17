aed65fa61ad2826b3e65f173dc29609d348cdc6a
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-05-17 12:39:22 +0000

    fixes #20

--- scripts/init-beta-sciencemesh.sh
+++ scripts/init-beta-sciencemesh.sh
@@ -12,10 +12,10 @@ docker pull pondersource/dev-stock-oc1-sciencemesh-network-beta:latest
 docker pull pondersource/dev-stock-oc2-sciencemesh-network-beta:latest
 
 # Nextcloud Sciencemesh source code.
-[ ! -d "nc-sciencemesh" ] && git clone --branch=sciencemesh  https://github.com/pondersource/nc-sciencemesh nc-sciencemesh && docker run -it -v "$(pwd)/nc-sciencemesh:/var/www/html/apps/sciencemesh" --workdir /var/www/html/apps/sciencemesh pondersource/dev-stock-nc1-sciencemesh make composer
+[ ! -d "nc-sciencemesh" ] && git clone --branch=sciencemesh  https://github.com/pondersource/nc-sciencemesh nc-sciencemesh && docker run -it -v "$(pwd)/nc-sciencemesh:/var/www/html/apps/sciencemesh" --workdir /var/www/html/apps/sciencemesh pondersource/dev-stock-nc1-sciencemesh-network-beta make composer
 
 # ownCloud Sciencemesh source code.
-[ ! -d "oc-sciencemesh" ] && git clone --branch=oc-10-take-2 https://github.com/pondersource/nc-sciencemesh oc-sciencemesh && docker run -it -v "$(pwd)/oc-sciencemesh:/var/www/html/apps/sciencemesh" --workdir /var/www/html/apps/sciencemesh pondersource/dev-stock-oc1-sciencemesh make composer
+[ ! -d "oc-sciencemesh" ] && git clone --branch=oc-10-take-2 https://github.com/pondersource/nc-sciencemesh oc-sciencemesh && docker run -it -v "$(pwd)/oc-sciencemesh:/var/www/html/apps/sciencemesh" --workdir /var/www/html/apps/sciencemesh pondersource/dev-stock-oc1-sciencemesh-network-beta make composer
 
 # Reva source code.
 [ ! -d "reva" ] && git clone --branch=sciencemesh-dev https://github.com/cs3org/reva && cd reva && make revad && cd ..
