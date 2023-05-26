c27d062c7873c2939e79f50cfd9500b16df84948
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-05-26 19:52:23 +0000

    fix: reva restart messing with sharing between efss

--- scripts/reva-kill-run.sh
+++ scripts/reva-kill-run.sh
@@ -2,23 +2,14 @@
 
 set -e
 
-REPO_ROOT=$(pwd)
-
 # get reva container names. (this assumes only 2 containers with reva in their names exist)
 REVA1=$(docker ps --filter "name=reva" --format "{{.Names}}" | tail -1)
 REVA2=$(docker ps --filter "name=reva" --format "{{.Names}}" | head -1)
 
-# stop revad containers.
-docker stop "${REVA1}"
-docker stop "${REVA2}"
+# kill reva.
+docker exec "${REVA1}" bash -c "reva-kill.sh"
+docker exec "${REVA2}" bash -c "reva-kill.sh"
 
-cd "${REPO_ROOT}/reva"
-
-# rebuild reva.
-go mod tidy
-go mod vendor
-make revad
-
-# start revad containers.
-docker start "${REVA1}"
-docker start "${REVA2}"
+# run revad.
+docker exec "${REVA1}" bash -c "reva-run.sh"
+docker exec "${REVA2}" bash -c "reva-run.sh"
--- scripts/reva-restart.sh
+++ scripts/reva-restart.sh
@@ -6,10 +6,5 @@ set -e
 REVA1=$(docker ps --filter "name=reva" --format "{{.Names}}" | tail -1)
 REVA2=$(docker ps --filter "name=reva" --format "{{.Names}}" | head -1)
 
-# kill reva.
-docker exec "${REVA1}" bash -c "reva-kill.sh"
-docker exec "${REVA2}" bash -c "reva-kill.sh"
-
-# run revad.
-docker exec "${REVA1}" bash -c "reva-run.sh"
-docker exec "${REVA2}" bash -c "reva-run.sh"
+docker restart "${REVA1}"
+docker restart "${REVA2}"
