563c6eb9de561d841573fceed815c89ab222e38a
Reza Soltani <soltanireza65@gmail.com>
2023-06-16 10:43:58 +0000

    adding testing-nc

---
+++ scripts/testing-nc.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+REPO_ROOT=$(pwd)
+export REPO_ROOT=$REPO_ROOT
+
+function waitForPort {
+  x=$(docker exec -it "${1}" ss -tulpn | grep -c "${2}")
+  until [ "${x}" -ne 0 ]
+  do
+    echo Waiting for "${1}" to open port "${2}", this usually takes about 10 seconds ... "${x}"
+    sleep 1
+    x=$(docker exec -it "${1}" ss -tulpn | grep -c "${2}")
+  done
+  echo "${1}" port "${2}" is open
+}
+
+# copy init files.
+
+# echo "starting firefox tester"
+docker run --detach --name=firefox        --network=testnet -p 5800:5800 --shm-size 2g jlesage/firefox:latest
+
+echo "starting maria1.docker"
+docker run --detach --network=testnet                                                               \
+  --name=maria1.docker                                                                              \
+  -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek                                 \
+  mariadb                                                                                           \
+  --transaction-isolation=READ-COMMITTED                                                            \
+  --binlog-format=ROW                                                                               \
+  --innodb-file-per-table=1                                                                         \
+  --skip-innodb-read-only-compressed
+
+echo "starting nc1.docker"
+docker run --detach --network=testnet                                                               \
+  --name=nc1.docker                                                                                 \
+  --publish 8080:80                                                                                 \
+  --add-host "host.docker.internal:host-gateway"                                                    \
+  -e HOST="nc1"                                                                                     \
+  -e DBHOST="maria1.docker"                                                                         \
+  -e USER="einstein"                                                                                \
+  -e PASS="relativity"                                                                              \
+  pondersource/dev-stock-nextcloud
+
+echo "starting maria2.docker"
+docker run --detach --network=testnet                                                               \
+  --name=maria2.docker                                                                              \
+  -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek                                 \
+  mariadb                                                                                           \
+  --transaction-isolation=READ-COMMITTED                                                            \
+  --binlog-format=ROW                                                                               \
+  --innodb-file-per-table=1                                                                         \
+  --skip-innodb-read-only-compressed
+
+echo "starting nc2.docker"
+docker run --detach --network=testnet                                                               \
+  --name=nc2.docker                                                                                 \
+  --publish 9080:80                                                                                 \
+  --add-host "host.docker.internal:host-gateway"                                                    \
+  -e HOST="nc2"                                                                                     \
+  -e DBHOST="maria2.docker"                                                                         \
+  -e USER="marie"                                                                                   \
+  -e PASS="radioactivity"                                                                           \
+  pondersource/dev-stock-nextcloud
+
+waitForPort maria1.docker 3306
+waitForPort nc1.docker 443
+
+echo "executing init.sh on nc1.docker"
+docker exec -u www-data nc1.docker sh /nc-init.sh
+
+waitForPort maria2.docker 3306
+waitForPort nc2.docker 443
+
+echo "executing init.sh on nc2.docker"
+docker exec -u www-data nc2.docker sh /nc-init.sh
