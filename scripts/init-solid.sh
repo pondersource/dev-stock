f98c011ab67f871e9a81e1b30a6b3a64549ae7bb
Michiel de Jong <michiel@unhosted.org>
2023-07-03 13:50:40 +0000

    build solid-nextcloud app

--- scripts/init-solid.sh
+++ scripts/init-solid.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+REPO_ROOT=$(pwd)
+export REPO_ROOT=$REPO_ROOT
+
 # repositories and branches.
 REPO_SOLID=https://github.com/pdsinterop/solid-nextcloud.git
 BRANCH_SOLID=support-nc-27
@@ -13,6 +16,8 @@ BRANCH_SOLID=support-nc-27
     --branch ${BRANCH_SOLID}                                                \
     ${REPO_SOLID}                                                           \
     solid-nextcloud
+
+docker run -it --rm -v "${REPO_ROOT}/solid-nextcloud:/var/www/html/apps/solid-nextcloud" --workdir /var/www/html/apps/solid-nextcloud/solid "pondersource/dev-stock-nextcloud-solid"  bash -c "composer install"
 
 docker network inspect testnet >/dev/null 2>&1 || docker network create testnet
 
