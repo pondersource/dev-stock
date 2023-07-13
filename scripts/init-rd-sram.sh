0e9c7fb1db2ce85b481f05e9e9ffe337062562d0
Reza Soltani <soltanireza65@gmail.com>
2023-07-13 11:23:30 +0330

    clone the oc core repo

--- scripts/init-rd-sram.sh
+++ scripts/init-rd-sram.sh
@@ -9,6 +9,10 @@ BRANCH_OCM=main
 REPO_RD_SRAM=https://github.com/surfnet/rd-sram-integration
 BRANCH_RD_SRAM=main
 
+
+REPO_CORE=https://github.com/pondersource/core
+BRANCH_CORE=accept-ocm-to-groups
+
 [ ! -d "ocm" ] &&                                                               \
     git clone                                                                   \
     --branch ${BRANCH_OCM}                                                      \
@@ -21,6 +25,9 @@ BRANCH_RD_SRAM=main
     ${REPO_RD_SRAM}                                                             \
     rd-sram
 
+# uncommnt if you need to clone oc/core repo
+# [ ! -d "core" ] && git clone --branch ${BRANCH_CORE} ${REPO_CORE} core
+  
 docker network inspect testnet >/dev/null 2>&1 || docker network create testnet
 
 [ ! -d "temp" ] && mkdir --parents temp
