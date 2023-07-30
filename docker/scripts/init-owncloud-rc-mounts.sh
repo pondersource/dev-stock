73730b3d389a688a4ebbaeee4265aff2fb64e71e
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-07-30 13:57:11 +0000

    add: redis cache setting to config.php

--- docker/scripts/init-owncloud-rc-mounts.sh
+++ docker/scripts/init-owncloud-rc-mounts.sh
@@ -1,35 +1,37 @@
 #!/usr/bin/env bash
 
-# create symbolic link (by force)
-ln --symbolic --force /var/www/html/apps/rc-mounts/tokenbasedav /var/www/html/apps/tokenbasedav
+# create symbolic link if it doesn't exists.
+if [[ ! -d "/var/www/html/apps/tokenbasedav" ]]; then
+    ln --symbolic --force /var/www/html/apps/rc-mounts/tokenbasedav /var/www/html/apps/tokenbasedav
+fi
 
 php console.php maintenance:install --admin-user "${USER}" --admin-pass "${PASS}" --database "mysql"            \
                                     --database-name "efss" --database-user "root" --database-host "$DBHOST"     \
                                     --database-pass "eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek"
+
 php console.php app:disable firstrunwizard
 
-sed -i "8 i\      1 => 'oc1.docker'," /var/www/html/config/config.php
-sed -i "9 i\      2 => 'oc2.docker'," /var/www/html/config/config.php
+sed -i "8 i\    1 => 'oc1.docker'," /var/www/html/config/config.php
+sed -i "9 i\    2 => 'oc2.docker'," /var/www/html/config/config.php
 
 # insert redis cache details into config.php
-cat <<EOT >> /var/www/html/config/config.php
-'filelocking.enabled' => true,
-'memcache.locking' => '\OC\Memcache\Redis',
-'memcache.local' => '\OC\Memcache\Redis',
-'memcache.distributed' => '\OC\Memcache\Redis',
-'redis' => [
-    'host' => '${REDIS_HOST}',  // For a Unix domain socket, use '/var/run/redis/redis.sock'
-    'port' => 6379,         // Set to 0 when using a Unix socket
-    'timeout' => 0,         // Optional, keep connection open forever
-    'password' => '',       // Optional, if not defined no password will be used.
-    'dbindex' => 0,         // Optional, if undefined SELECT will not run and will
-                            // use Redis Server's default DB Index.
-],
-EOT
+sed -i "40 i\  'filelocking.enabled' => true,"                          /var/www/html/config/config.php
+sed -i "41 i\  'memcache.locking' => '\\OC\\Memcache\\Redis',"          /var/www/html/config/config.php
+sed -i "42 i\  'memcache.local' => '\\OC\\Memcache\\Redis',"            /var/www/html/config/config.php
+sed -i "43 i\  'memcache.distributed' => '\\OC\\Memcache\\Redis',"      /var/www/html/config/config.php
+sed -i "44 i\  'redis' => ["                                            /var/www/html/config/config.php
+sed -i "45 i\      'host' => '${REDIS_HOST}',"                          /var/www/html/config/config.php
+sed -i "46 i\      'port' => 6379,"                                     /var/www/html/config/config.php
+sed -i "47 i\      'timeout' => 0,"                                     /var/www/html/config/config.php
+sed -i "48 i\      'password' => '',"                                   /var/www/html/config/config.php
+sed -i "49 i\      'dbindex' => 0,"                                     /var/www/html/config/config.php
+sed -i "50 i\  ],"                                                      /var/www/html/config/config.php
+
+# some how above command doesn't have the corrct backslash escaping and we have to do it again! 
+sed -i 's/OCMemcacheRedis/\\OC\\Memcache\\Redis/g' /var/www/html/config/config.php
 
 echo "Installing DAV Token Access"
 php console.php app:enable tokenbasedav
 
 echo "Installing Open ID Connect"
-cd make /var/www/html/apps/openidconnect && make install-php-deps
-cd /var/www/html && php console.php app:enable openidconnect
+php console.php app:enable openidconnect
