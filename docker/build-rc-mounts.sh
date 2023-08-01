5c95602b3ec6d6093858d0fd209102604202ca0c
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-08-01 14:39:59 +0000

    fix: typo

--- docker/build-rc-mounts.sh
+++ docker/build-rc-mounts.sh
@@ -14,7 +14,7 @@ echo Building pondersource/dev-stock-owncloud
 docker build --build-arg CACHEBUST="default" --file ./owncloud.Dockerfile --tag pondersource/dev-stock-owncloud .
 
 echo Building pondersource/dev-stock-owncloud-rc-mounts
-docker build --build-arg CACHEBUST="default" --file ./owncloud-rc-mounts.Dockerfile --tag pondersource/dev-stock-owncloudrc-mounts .
+docker build --build-arg CACHEBUST="default" --file ./owncloud-rc-mounts.Dockerfile --tag pondersource/dev-stock-owncloud-rc-mounts .
 
 # remove all <none> images.
 # docker images -a | grep none | awk '{ print $3; }' | xargs docker rmi --force
