5b13eeaabb327fae11699c14950b1f03d69fbff9
Reza Soltani <soltanireza65@gmail.com>
2023-08-24 08:40:42 +0000

    update the bootstrap-rd-sram script

--- bootstrap-rd-sram.sh
+++ bootstrap-rd-sram.sh
@@ -10,5 +10,7 @@ docker exec -it oc2.docker sh /curls/includeMarie.sh oc2.docker
 
 docker exec -it oc1.docker sh /curls/includeMarie.sh oc1.docker
 
+# be aware that these optional command is going to break the occ command [https://github.com/SURFnet/rd-sram-integration/issues/237]
+
 docker exec -it oc1.docker sed -i "14 i\      3 => \$_SERVER['HTTP_HOST']," /var/www/html/config/config.php
 docker exec -it oc2.docker sed -i "14 i\      3 => \$_SERVER['HTTP_HOST']," /var/www/html/config/config.php
