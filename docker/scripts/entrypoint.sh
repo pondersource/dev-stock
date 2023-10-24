7ec33f5e34a49eeb04c8050ca9f9d58135fd16e4
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-10-24 07:50:30 +0000

    remove: space

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -4,8 +4,8 @@
 set -e
 
 mkdir -p /tls
-ln --symbolic --force "/tls-host/${HOST}.crt"   /tls/server.cert
-ln --symbolic --force "/tls-host/${HOST}.key"   /tls/server.key
+ln --symbolic --force "/tls-host/${HOST}.crt" /tls/server.cert
+ln --symbolic --force "/tls-host/${HOST}.key" /tls/server.key
 
 # This will exec the CMD from your Dockerfile, i.e. "npm start"
 exec "$@"
