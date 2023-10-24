cc1fffc344954a1ece9c531298b1b03cf2c192f8
Michiel de Jong <michiel@unhosted.org>
2023-10-24 12:29:20 +0200

    Update bootstrap-rd-sram.sh

--- bootstrap-rd-sram.sh
+++ bootstrap-rd-sram.sh
@@ -10,7 +10,5 @@ docker exec -it oc2.docker sh /curls/includeMarie.sh oc2.docker
 
 docker exec -it oc1.docker sh /curls/includeMarie.sh oc1.docker
 
-# be aware that these optional command is going to break the occ command [https://github.com/SURFnet/rd-sram-integration/issues/237]
-
 docker exec -it oc1.docker sed -i "14 i\      3 => (isset(\$_SERVER['HTTP_HOST']) ? \$_SERVER['HTTP_HOST'] : 'localhost')," /var/www/html/config/config.php
 docker exec -it oc2.docker sed -i "14 i\      3 => (isset(\$_SERVER['HTTP_HOST']) ? \$_SERVER['HTTP_HOST'] : 'localhost')," /var/www/html/config/config.php
