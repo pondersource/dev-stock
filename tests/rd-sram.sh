b3c4710e39140f553ff7f88576cb48ced9caf06a
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-10-31 10:44:55 +0000

    refactor: whole rd-sram test script

--- tests/rd-sram.sh
+++ tests/rd-sram.sh
@@ -3,6 +3,9 @@
 # @michielbdejong halt on error in docker init scripts
 set -e
 
+export EFSS1=owncloud
+export EFSS2=owncloud
+
 # find this scripts location.
 SOURCE=${BASH_SOURCE[0]}
 while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -35,7 +38,6 @@ function waitForPort () {
 [ ! -d "${ENV_ROOT}/temp" ] && mkdir -p "${ENV_ROOT}/temp"
 
 # copy init files.
-cp --force --recursive ./docker/rd-sram/curls ./temp/curls
 cp --force "${ENV_ROOT}/docker/scripts/init-owncloud-rd-sram.sh" "${ENV_ROOT}/temp/oc-rd-sram.sh"
 
 docker run --detach --name=firefox          --network=testnet -p 5800:5800  --shm-size 2g jlesage/firefox:latest
@@ -51,19 +53,23 @@ docker run --detach --network=testnet                                           
   --innodb-file-per-table=1                                                                         \
   --skip-innodb-read-only-compressed
 
-echo "starting oc1.docker"
+echo "starting ${EFSS1}1.docker"
 docker run --detach --network=testnet                                                               \
-  --name=oc1.docker                                                                                 \
+  --name="${EFSS1}1.docker"                                                                         \
   --publish 8080:80                                                                                 \
   --add-host "host.docker.internal:host-gateway"                                                    \
-  -e HOST="oc1"                                                                                     \
+  -e HOST="${EFSS1}1"                                                                               \
   -e DBHOST="maria1.docker"                                                                         \
   -e USER="einstein"                                                                                \
   -e PASS="relativity"                                                                              \
-  -v "${ENV_ROOT}/temp/oc-rd-sram.sh:/init.sh"                                                     \
-  -v "${ENV_ROOT}/rd-sram:/var/www/html/apps/rd-sram-integration"                                  \
-  -v "${ENV_ROOT}/ocm:/var/www/html/apps/oc-opencloudmesh"                                         \
-  -v "${ENV_ROOT}/docker/rd-sram/curls:/curls"                                                     \
+  -v "${ENV_ROOT}/docker/tls:/tls-host"                                                             \
+  -v "${ENV_ROOT}/temp/oc-rd-sram.sh:/init.sh"                                                      \
+  -v "${ENV_ROOT}/docker/scripts/entrypoint.sh:/entrypoint.sh"                                      \
+  -v "${ENV_ROOT}/docker/rd-sram/curls:/curls"                                                      \
+  -v "${ENV_ROOT}/${EFSS1}/apps/sciencemesh:/var/www/html/apps/sciencemesh"                         \
+  -v "${ENV_ROOT}/${EFSS1}/apps/customgroups:/var/www/html/apps/customgroups"                       \
+  -v "${ENV_ROOT}/${EFSS1}/apps/opencloudmesh:/var/www/html/apps/opencloudmesh"                     \
+  -v "${ENV_ROOT}/${EFSS1}/apps/federatedgroups:/var/www/html/apps/federatedgroups"                 \
   pondersource/dev-stock-owncloud-rd-sram
 
 echo "starting maria2.docker"
@@ -76,42 +82,46 @@ docker run --detach --network=testnet                                           
   --innodb-file-per-table=1                                                                         \
   --skip-innodb-read-only-compressed
 
-echo "starting oc2.docker"
+echo "starting ${EFSS2}2.docker"
 docker run --detach --network=testnet                                                               \
-  --name=oc2.docker                                                                                 \
+  --name="${EFSS2}2.docker"                                                                         \
   --publish 9080:80                                                                                 \
   --add-host "host.docker.internal:host-gateway"                                                    \
-  -e HOST="oc2"                                                                                     \
+  -e HOST="${EFSS2}2"                                                                               \
   -e DBHOST="maria2.docker"                                                                         \
   -e USER="marie"                                                                                   \
   -e PASS="radioactivity"                                                                           \
-  -v "${ENV_ROOT}/temp/oc-rd-sram.sh:/init.sh"                                                     \
-  -v "${ENV_ROOT}/rd-sram:/var/www/html/apps/rd-sram-integration"                                  \
-  -v "${ENV_ROOT}/ocm:/var/www/html/apps/oc-opencloudmesh"                                         \
-  -v "${ENV_ROOT}/docker/rd-sram/curls:/curls"                                                     \
+  -v "${ENV_ROOT}/docker/tls:/tls-host"                                                             \
+  -v "${ENV_ROOT}/temp/oc-rd-sram.sh:/init.sh"                                                      \
+  -v "${ENV_ROOT}/docker/scripts/entrypoint.sh:/entrypoint.sh"                                      \
+  -v "${ENV_ROOT}/docker/rd-sram/curls:/curls"                                                      \
+  -v "${ENV_ROOT}/${EFSS1}/apps/sciencemesh:/var/www/html/apps/sciencemesh"                         \
+  -v "${ENV_ROOT}/${EFSS1}/apps/customgroups:/var/www/html/apps/customgroups"                       \
+  -v "${ENV_ROOT}/${EFSS1}/apps/opencloudmesh:/var/www/html/apps/opencloudmesh"                     \
+  -v "${ENV_ROOT}/${EFSS1}/apps/federatedgroups:/var/www/html/apps/federatedgroups"                 \
   pondersource/dev-stock-owncloud-rd-sram
 
 waitForPort maria1.docker 3306
-waitForPort oc1.docker 443
+waitForPort ${EFSS1}1.docker 443
 
-docker exec "oc1.docker" bash -c "cp /tls/*.crt /usr/local/share/ca-certificates/"
-docker exec "oc1.docker" bash -c "cp /tls-host/*.crt /usr/local/share/ca-certificates/"
-docker exec "oc1.docker" update-ca-certificates
-docker exec "oc1.docker" bash -c "cat /etc/ssl/certs/ca-certificates.crt >> /var/www/html/resources/config/ca-bundle.crt"
+docker exec "${EFSS1}1.docker" bash -c "cp /tls/*.crt /usr/local/share/ca-certificates/"
+docker exec "${EFSS1}1.docker" bash -c "cp /tls-host/*.crt /usr/local/share/ca-certificates/"
+docker exec "${EFSS1}1.docker" update-ca-certificates
+docker exec "${EFSS1}1.docker" bash -c "cat /etc/ssl/certs/ca-certificates.crt >> /var/www/html/resources/config/ca-bundle.crt"
 
-echo "executing init.sh on oc1.docker"
-docker exec -u www-data oc1.docker bash /init.sh
+echo "executing init.sh on ${EFSS1}1.docker"
+docker exec -u www-data ${EFSS1}1.docker bash /init.sh
 
 waitForPort maria2.docker 3306
-waitForPort oc2.docker 443
+waitForPort ${EFSS2}2.docker 443
 
-docker exec "oc2.docker" bash -c "cp /tls/*.crt /usr/local/share/ca-certificates/"
-docker exec "oc2.docker" bash -c "cp /tls-host/*.crt /usr/local/share/ca-certificates/"
-docker exec "oc2.docker" update-ca-certificates
-docker exec "oc2.docker" bash -c "cat /etc/ssl/certs/ca-certificates.crt >> /var/www/html/resources/config/ca-bundle.crt"
+docker exec "${EFSS2}2.docker" bash -c "cp /tls/*.crt /usr/local/share/ca-certificates/"
+docker exec "${EFSS2}2.docker" bash -c "cp /tls-host/*.crt /usr/local/share/ca-certificates/"
+docker exec "${EFSS2}2.docker" update-ca-certificates
+docker exec "${EFSS2}2.docker" bash -c "cat /etc/ssl/certs/ca-certificates.crt >> /var/www/html/resources/config/ca-bundle.crt"
 
-echo "executing init.sh on oc2.docker"
-docker exec -u www-data oc2.docker bash /init.sh
+echo "executing init.sh on ${EFSS2}2.docker"
+docker exec -u www-data ${EFSS2}2.docker bash /init.sh
 
 echo "Setting up SCIM control for Federated Groups"
 mysql1_cmd="docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss"
@@ -120,17 +130,20 @@ mysql2_cmd="docker exec maria2.docker mariadb -u root -peilohtho9oTahsuongeeTh7r
 $mysql1_cmd -e "insert into oc_appconfig (appid, configkey, configvalue) VALUES ('federatedgroups', 'scim_token', 'something-super-secret');"
 $mysql2_cmd -e "insert into oc_appconfig (appid, configkey, configvalue) VALUES ('federatedgroups', 'scim_token', 'something-super-secret');"
 
-echo "Creating federated group 'TestGroup (uniharderwijk_surfdrive_test) (SRAM CO)' on oc1"
-docker exec -it oc1.docker sh /curls/createGroup.sh oc1.docker
+echo "Creating federated group 'TestGroup (uniharderwijk_surfdrive_test) (SRAM CO)' on ${EFSS1}1"
+docker exec -it ${EFSS1}1.docker sh /curls/createGroup.sh ${EFSS1}1.docker
 
-echo "Creating federated group 'TestGroup (uniharderwijk_surfdrive_test) (SRAM CO)' on oc2"
-docker exec -it oc2.docker sh /curls/createGroup.sh oc2.docker
+echo "Creating federated group 'TestGroup (uniharderwijk_surfdrive_test) (SRAM CO)' on ${EFSS2}"
+docker exec -it ${EFSS2}2.docker sh /curls/createGroup.sh ${EFSS2}2.docker
 
-docker exec -it oc1.docker sh /curls/excludeMarie.sh oc1.docker
-docker exec -it oc2.docker sh /curls/excludeMarie.sh oc2.docker
+docker exec -it ${EFSS1}1.docker sh /curls/excludeMarie.sh ${EFSS1}1.docker
+docker exec -it ${EFSS2}2.docker sh /curls/excludeMarie.sh ${EFSS2}2.docker
 
-echo "share something from einstein@oc1.docker to Test Group, then run:"
-echo "$ docker exec -it oc2.docker sh /curls/includeMarie.sh oc2.docker"
-echo "$ docker exec -it oc1.docker sh /curls/includeMarie.sh oc1.docker"
-echo "then log in to oc2.docker as marie, you should not have received the share"
-echo "refresh the oc2.docker page, the share from einstein to Test Group should now also arrive to Marie"
+echo ""
+echo ""
+echo "RD-SRAM app instructions:"
+echo "share something from einstein@${EFSS1}1.docker to Test Group, then run:"
+echo "$ docker exec -it ${EFSS2}2.docker sh /curls/includeMarie.sh ${EFSS2}2.docker"
+echo "$ docker exec -it ${EFSS1}1.docker sh /curls/includeMarie.sh ${EFSS1}1.docker"
+echo "then log in to ${EFSS2}2.docker as marie, you should not have received the share"
+echo "refresh the ${EFSS2}2.docker page, the share from einstein to Test Group should now also arrive to Marie"
