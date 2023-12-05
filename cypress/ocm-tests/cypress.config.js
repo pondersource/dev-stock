dac8a2932f0bbd03a3400f4bfe7425762a082e22
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2023-12-05 13:08:11 +0000

    add: cypress record

--- cypress/ocm-tests/cypress.config.js
+++ cypress/ocm-tests/cypress.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
-};
+  video: true,
+  videoCompression: true,
+})
