9e5011849600484cf60d65b97e0e6654db0f2e66
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2024-02-19 13:57:18 +0330

    fix: image name

--- docker/build/all.sh
+++ docker/build/all.sh
@@ -69,4 +69,4 @@ echo Building pondersource/dev-stock-owncloud-rd-sram
 docker build --build-arg CACHEBUST="default" --file ./dockerfiles/owncloud-rd-sram.Dockerfile --tag pondersource/dev-stock-owncloud-rd-sram .
 
 echo Building pondersource/dev-stock-owncloud-ocm-test-suite
-docker build --build-arg CACHEBUST="default" --file ./dockerfiles/owncloud-ocm-test-suite.Dockerfile --tag pondersource/dev-stock-ocm-test-suite .
+docker build --build-arg CACHEBUST="default" --file ./dockerfiles/owncloud-ocm-test-suite.Dockerfile --tag pondersource/dev-stock-owncloud-ocm-test-suite .
