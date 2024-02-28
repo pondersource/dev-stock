34da8f109c48b3fede49c5e673195e6da8e3092d
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-02-28 09:42:41 +0000

    feat: make sure directory exists before cd command

--- release/sciencemesh-nextcloud.sh
+++ release/sciencemesh-nextcloud.sh
@@ -19,84 +19,88 @@ cp -f ./docker/scripts/init-nextcloud-sciencemesh.sh ./temp/nc.sh
 docker tag pondersource/dev-stock-nextcloud-sciencemesh pondersource/dev-stock-nc1-sciencemesh
 
 # ownCloud Sciencemesh source code.
-[ ! -d "nc-sciencemesh-release" ] &&                                            \
-    git clone                                                                   \
-    --depth 1                                                                   \
-    --branch ${BRANCH_OWNCLOUD_APP}                                             \
-    ${REPO_OWNCLOUD_APP}                                                        \
-    nc-sciencemesh-release                                                      \
-    &&                                                                          \
-    docker run -it                                                              \
-    -v "${REPO_ROOT}/nc-sciencemesh-release:/var/www/html/apps/sciencemesh"     \
-    --workdir /var/www/html/apps/sciencemesh                                    \
-    pondersource/dev-stock-nc1-sciencemesh                                      \
+[ ! -d "nc-sciencemesh-release" ] &&                                                                              \
+    git clone                                                                                                     \
+    --depth 1                                                                                                     \
+    --branch ${BRANCH_OWNCLOUD_APP}                                                                               \
+    ${REPO_OWNCLOUD_APP}                                                                                          \
+    nc-sciencemesh-release                                                                                        \
+    &&                                                                                                            \
+    docker run -it                                                                                                \
+    -v "${REPO_ROOT}/nc-sciencemesh-release:/var/www/html/apps/sciencemesh"                                       \
+    --workdir /var/www/html/apps/sciencemesh                                                                      \
+    pondersource/dev-stock-nc1-sciencemesh                                                                        \
     make composer
 
 "${REPO_ROOT}/release/tag-release.py" nc none nc-sciencemesh-release
 
-docker run --detach --network=testnet                                           \
-  --name=maria1.docker                                                          \
-  -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek             \
-  mariadb                                                                       \
-  --transaction-isolation=READ-COMMITTED                                        \
-  --binlog-format=ROW                                                           \
-  --innodb-file-per-table=1                                                     \
+docker run --detach --network=testnet                                                                             \
+  --name=maria1.docker                                                                                            \
+  -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek                                               \
+  mariadb                                                                                                         \
+  --transaction-isolation=READ-COMMITTED                                                                          \
+  --binlog-format=ROW                                                                                             \
+  --innodb-file-per-table=1                                                                                       \
   --skip-innodb-read-only-compressed
 
-docker run --detach --network=testnet                                           \
-  --name=nc-release.docker                                                      \
-  --add-host "host.docker.internal:host-gateway"                                \
-  -e HOST="nc1"                                                                 \
-  -e DBHOST="maria1.docker"                                                     \
-  -e USER="einstein"                                                            \
-  -e PASS="relativity"                                                          \
-  -v "${REPO_ROOT}/temp/nc.sh:/nc-init.sh"                                      \
-  -v "${REPO_ROOT}/nc-sciencemesh-release:/var/www/html/apps/sciencemesh"       \
-  -v "${REPO_ROOT}/release/sciencemesh.key:/var/www/sciencemesh.key"            \
+docker run --detach --network=testnet                                                                             \
+  --name=nc-release.docker                                                                                        \
+  --add-host "host.docker.internal:host-gateway"                                                                  \
+  -e HOST="nc1"                                                                                                   \
+  -e DBHOST="maria1.docker"                                                                                       \
+  -e USER="einstein"                                                                                              \
+  -e PASS="relativity"                                                                                            \
+  -v "${REPO_ROOT}/temp/nc.sh:/nc-init.sh"                                                                        \
+  -v "${REPO_ROOT}/nc-sciencemesh-release:/var/www/html/apps/sciencemesh"                                         \
+  -v "${REPO_ROOT}/release/sciencemesh.key:/var/www/sciencemesh.key"                                              \
   "pondersource/dev-stock-nextcloud-sciencemesh"
 
-docker exec --user root nc-release.docker bash -c "chown www-data:www-data -R /var/www/html/apps/sciencemesh && chown www-data:www-data /var/www/sciencemesh.key"
-docker exec --user www-data nc-release.docker bash -c "cd /var/www/html/apps/sciencemesh                \
-                                                    &&                                                  \
-                                                    mkdir -p build/sciencemesh                          \
-                                                    &&                                                  \
-                                                    rm -rf build/sciencemesh/*                          \
-                                                    &&                                                  \
-                                                    cp -r appinfo build/sciencemesh/                    \
-                                                    &&                                                  \
-                                                    cp -r css build/sciencemesh/                        \
-                                                    &&                                                  \
-                                                    cp -r img build/sciencemesh/                        \
-                                                    &&                                                  \
-                                                    cp -r js build/sciencemesh/                         \
-                                                    &&                                                  \
-                                                    cp -r lib build/sciencemesh/                        \
-                                                    &&                                                  \
-                                                    cp -r templates build/sciencemesh/                  \
-                                                    &&                                                  \
-                                                    cp -r composer.* build/sciencemesh/                 \
-                                                    &&                                                  \
-                                                    cd build/sciencemesh/                               \
-                                                    &&                                                  \
-                                                    composer install                                    \
-                                                    &&                                                  \
-                                                    cd ..                                               \
-                                                    &&                                                  \
-                                                    tar -cf sciencemesh.tar sciencemesh"
+docker exec --user root nc-release.docker bash -c "chown www-data:www-data -R /var/www/html/apps/sciencemesh      \
+                                                  &&                                                              \
+                                                  chown www-data:www-data /var/www/sciencemesh.key"
+
+docker exec --user www-data nc-release.docker bash -c "cd /var/www/html/apps/sciencemesh                          \
+                                                      &&                                                          \
+                                                      mkdir -p build/sciencemesh                                  \
+                                                      &&                                                          \
+                                                      rm -rf build/sciencemesh/*                                  \
+                                                      &&                                                          \
+                                                      cp -r appinfo build/sciencemesh/                            \
+                                                      &&                                                          \
+                                                      cp -r css build/sciencemesh/                                \
+                                                      &&                                                          \
+                                                      cp -r img build/sciencemesh/                                \
+                                                      &&                                                          \
+                                                      cp -r js build/sciencemesh/                                 \
+                                                      &&                                                          \
+                                                      cp -r lib build/sciencemesh/                                \
+                                                      &&                                                          \
+                                                      cp -r templates build/sciencemesh/                          \
+                                                      &&                                                          \
+                                                      cp -r composer.* build/sciencemesh/                         \
+                                                      &&                                                          \
+                                                      cd build/sciencemesh/                                       \
+                                                      &&                                                          \
+                                                      composer install                                            \
+                                                      &&                                                          \
+                                                      cd ..                                                       \
+                                                      &&                                                          \
+                                                      tar -cf sciencemesh.tar sciencemesh"
 
 echo "NextCloud Signature start:"
-docker exec --user root nc-release.docker bash -c "cd /var/www/html/apps/sciencemesh/release            \
-                                                    &&                                                  \
-                                                    mv ../build/sciencemesh.tar .                       \
-                                                    &&                                                  \
-                                                    rm -f -- sciencemesh.tar.gz                         \
-                                                    &&                                                  \
-                                                    gzip sciencemesh.tar                                \
-                                                    &&                                                  \
-                                                    openssl dgst                                        \
-                                                    -sha512                                             \
-                                                    -sign /var/www/sciencemesh.key                      \
-                                                    ./sciencemesh.tar.gz | openssl base64"
+docker exec --user root nc-release.docker bash -c "mkdir -p /var/www/html/apps/sciencemesh/release                \
+                                                  cd /var/www/html/apps/sciencemesh/release                       \
+                                                  &&                                                              \
+                                                  mv ../build/sciencemesh.tar .                                   \
+                                                  &&                                                              \
+                                                  rm -f -- sciencemesh.tar.gz                                     \
+                                                  &&                                                              \
+                                                  gzip sciencemesh.tar                                            \
+                                                  &&                                                              \
+                                                  openssl dgst                                                    \
+                                                  -sha512                                                         \
+                                                  -sign /var/www/sciencemesh.key                                  \
+                                                  ./sciencemesh.tar.gz | openssl base64"
 echo "NextCloud Signature end"
 
 "${REPO_ROOT}/scripts/clean.sh"
