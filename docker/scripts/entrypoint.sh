18f462d89767f7255767df2765a185bc6cf8a571
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-03-04 09:24:55 +0000

    modify: copy and link certificates

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-# @michielbdejong halt on error in docker init scripts
+# @michielbdejong halt on error in docker init scripts.
 set -e
 
 mkdir -p /tls
-ln --symbolic --force "/tls-host/${HOST}.crt" /tls/server.cert
-ln --symbolic --force "/tls-host/${HOST}.key" /tls/server.key
+
+[ -d "/tls-host" ] &&                                                           \
+  cp -f /tls-host/*.crt                   /tls/                                 \
+  &&                                                                            \
+  cp -f /tls-host/*.key                   /tls/
+
+ln --symbolic --force "/tls/${HOST}.crt"    /tls/server.crt
+ln --symbolic --force "/tls/${HOST}.key"    /tls/server.key
 
 # This will exec the CMD from your Dockerfile, i.e. "npm start"
 exec "$@"
