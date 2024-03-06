17f6e1209aad383d27d541892554c64517558f37
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-03-06 14:15:57 +0000

    add: ability to run both dev and ci

--- dev/ocm-test-suite.sh
+++ dev/ocm-test-suite.sh
@@ -18,28 +18,43 @@ cd "${DIR}/.." || exit
 ENV_ROOT=$(pwd)
 export ENV_ROOT=${ENV_ROOT}
 
+# script mode:   dev, ci. default is dev.
+SCRIPT_MODE=${1:-"dev"}
+
+# test platform: chrome, edge, firefox, electron. default is chrome.
+# only applies on SCRIPT_MODE=ci
+TEST_PLATFORM=${2:-"chrome"}
+
+function redirect_to_null_cmd() {
+    if [ "${SCRIPT_MODE}" = "ci" ]; then
+        "$@" >/dev/null 2>&1
+    else
+        "$@"
+    fi
+}
+
 function waitForPort () {
-  echo waitForPort "${1}" "${2}"
+  redirect_to_null_cmd echo waitForPort "${1} ${2}"
   # the "| cat" after the "| grep" is to prevent the command from exiting with 1 if no match is found by grep.
   x=$(docker exec -it "${1}" ss -tulpn | grep -c "${2}" | cat)
   until [ "${x}" -ne 0 ]
   do
-    echo Waiting for "${1}" to open port "${2}", this usually takes about 10 seconds ... "${x}"
+    redirect_to_null_cmd echo Waiting for "${1} to open port ${2}, this usually takes about 10 seconds ... ${x}"
     sleep 1
     x=$(docker exec -it "${1}" ss -tulpn | grep -c "${2}" |  cat)
   done
-  echo "${1}" port "${2}" is open
+  redirect_to_null_cmd echo "${1} port ${2} is open"
 }
 
 function waitForCollabora() {
   x=$(docker logs collabora.docker | grep -c "Ready")
   until [ "${x}" -ne 0 ]
   do
-    echo Waiting for Collabora to be ready, this usually takes about 10 seconds ... "${x}"
+    redirect_to_null_cmd echo "Waiting for Collabora to be ready, this usually takes about 10 seconds ... ${x}"
     sleep 1
     x=$(docker logs collabora.docker | grep -c "Ready")
   done
-  echo "Collabora is ready"
+  redirect_to_null_cmd echo "Collabora is ready"
 }
 
 function createEfss() {
@@ -55,18 +70,18 @@ function createEfss() {
     local image="pondersource/dev-stock-${platform}-${image}"
   fi
 
-  echo "creating efss ${platform} ${number}"
+  redirect_to_null_cmd echo "creating efss ${platform} ${number}"
 
-  docker run --detach --network=testnet                                           \
+  redirect_to_null_cmd docker run --detach --network=testnet                      \
     --name="maria${platform}${number}.docker"                                     \
     -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek             \
     mariadb                                                                       \
     --transaction-isolation=READ-COMMITTED                                        \
     --binlog-format=ROW                                                           \
     --innodb-file-per-table=1                                                     \
-    --skip-innodb-read-only-compressed                                            \
+    --skip-innodb-read-only-compressed
 
-  docker run --detach --network=testnet                                           \
+  redirect_to_null_cmd docker run --detach --network=testnet                      \
     --name="${platform}${number}.docker"                                          \
     --add-host "host.docker.internal:host-gateway"                                \
     -e HOST="${platform}${number}"                                                \
@@ -80,21 +95,21 @@ function createEfss() {
     -v "${ENV_ROOT}/${platform}/apps/sciencemesh:/var/www/html/apps/sciencemesh"  \
     "${image}"
 
-    # wait for hostname port to be open.
-    waitForPort "maria${platform}${number}.docker"  3306
-    waitForPort "${platform}${number}.docker"       443
+  # wait for hostname port to be open.
+  waitForPort "maria${platform}${number}.docker"  3306
+  waitForPort "${platform}${number}.docker"       443
 
-    # add self-signed certificates to os and trust them. (use >/dev/null 2>&1 to shut these up)
-    docker exec "${platform}${number}.docker" bash -c "cp -f /certificates/*.crt                    /usr/local/share/ca-certificates/ || true"            >/dev/null 2>&1
-    docker exec "${platform}${number}.docker" bash -c "cp -f /certificate-authority/*.crt           /usr/local/share/ca-certificates/ || true"            >/dev/null 2>&1
-    docker exec "${platform}${number}.docker" bash -c "cp -f /tls/*.crt                             /usr/local/share/ca-certificates/ || true"            >/dev/null 2>&1
-    docker exec "${platform}${number}.docker" update-ca-certificates                                                                                      >/dev/null 2>&1
-    docker exec "${platform}${number}.docker" bash -c "cat /etc/ssl/certs/ca-certificates.crt >> /var/www/html/resources/config/ca-bundle.crt"            >/dev/null 2>&1
+  # add self-signed certificates to os and trust them. (use >/dev/null 2>&1 to shut these up)
+  docker exec "${platform}${number}.docker" bash -c "cp -f /certificates/*.crt                    /usr/local/share/ca-certificates/ || true"            >/dev/null 2>&1
+  docker exec "${platform}${number}.docker" bash -c "cp -f /certificate-authority/*.crt           /usr/local/share/ca-certificates/ || true"            >/dev/null 2>&1
+  docker exec "${platform}${number}.docker" bash -c "cp -f /tls/*.crt                             /usr/local/share/ca-certificates/ || true"            >/dev/null 2>&1
+  docker exec "${platform}${number}.docker" update-ca-certificates                                                                                      >/dev/null 2>&1
+  docker exec "${platform}${number}.docker" bash -c "cat /etc/ssl/certs/ca-certificates.crt >> /var/www/html/resources/config/ca-bundle.crt"            >/dev/null 2>&1
 
-    # run init script inside efss.
-    docker exec -u www-data "${platform}${number}.docker" sh "/${platform}-init.sh"
+  # run init script inside efss.
+  redirect_to_null_cmd docker exec -u www-data "${platform}${number}.docker" sh "/${platform}-init.sh"
 
-    echo ""
+  redirect_to_null_cmd echo ""
 }
 
 function createReva() {
@@ -102,7 +117,7 @@ function createReva() {
   local number="${2}"
   local port="${3}"
 
-  echo "creating reva for ${platform} ${number}"
+  redirect_to_null_cmd echo "creating reva for ${platform} ${number}"
 
   # make sure scripts are executable.
   chmod +x "${ENV_ROOT}/docker/scripts/reva-run.sh"           >/dev/null 2>&1
@@ -130,7 +145,7 @@ function sciencemeshInsertIntoDB() {
   local platform="${1}"
   local number="${2}"
 
-  echo "configuring ScienceMesh app for efss ${platform} ${number}"
+  redirect_to_null_cmd echo "configuring ScienceMesh app for efss ${platform} ${number}"
 
   # run db injections.
   mysql_cmd="docker exec "maria${platform}${number}.docker" mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss"
@@ -214,72 +229,84 @@ sciencemeshInsertIntoDB nextcloud 1
 sciencemeshInsertIntoDB nextcloud 2
 
 # Mesh directory for ScienceMesh invite flow.
-docker run --detach --network=testnet                                         \
-  --name=meshdir.docker                                                       \
-  -e HOST="meshdir"                                                           \
-  -v "${ENV_ROOT}/docker/scripts/stub.js:/ocm-stub/stub.js"                   \
-  pondersource/dev-stock-ocmstub                                              \
+docker run --detach --network=testnet                                                                       \
+  --name=meshdir.docker                                                                                     \
+  -e HOST="meshdir"                                                                                         \
+  -v "${ENV_ROOT}/docker/scripts/stub.js:/ocm-stub/stub.js"                                                 \
+  pondersource/dev-stock-ocmstub                                                                            \
   >/dev/null 2>&1
 
-###############
-### Firefox ###
-###############
+if [ "${SCRIPT_MODE}" = "dev" ]; then
+  ###############
+  ### Firefox ###
+  ###############
 
-docker run --detach --network=testnet                                                                     \
-  --name=firefox                                                                                          \
-  -p 5800:5800                                                                                            \
-  --shm-size 2g                                                                                           \
-  -e USER_ID="${UID}"                                                                                     \
-  -e GROUP_ID="${UID}"                                                                                    \
-  -e DARK_MODE=1                                                                                          \
-  -v "${ENV_ROOT}/docker/tls/browsers/firefox/cert9.db:/config/profile/cert9.db:rw"                       \
-  -v "${ENV_ROOT}/docker/tls/browsers/firefox/cert_override.txt:/config/profile/cert_override.txt:rw"     \
-  jlesage/firefox:latest                                                                                  \
-  >/dev/null 2>&1
+  docker run --detach --network=testnet                                                                     \
+    --name=firefox                                                                                          \
+    -p 5800:5800                                                                                            \
+    --shm-size 2g                                                                                           \
+    -e USER_ID="${UID}"                                                                                     \
+    -e GROUP_ID="${UID}"                                                                                    \
+    -e DARK_MODE=1                                                                                          \
+    -v "${ENV_ROOT}/docker/tls/browsers/firefox/cert9.db:/config/profile/cert9.db:rw"                       \
+    -v "${ENV_ROOT}/docker/tls/browsers/firefox/cert_override.txt:/config/profile/cert_override.txt:rw"     \
+    jlesage/firefox:latest                                                                                  \
+    >/dev/null 2>&1
 
-##################
-### VNC Server ###
-##################
+  ##################
+  ### VNC Server ###
+  ##################
 
-# remove previous x11 unix socket file, avoid any problems while mounting new one.
-sudo rm -rf "${ENV_ROOT}/temp/.X11-unix"
+  # remove previous x11 unix socket file, avoid any problems while mounting new one.
+  sudo rm -rf "${ENV_ROOT}/temp/.X11-unix"
 
-# try to change DISPLAY_WIDTH, DISPLAY_HEIGHT to make it fit in your screen,
-# NOTE: please do not commit any change related to resolution.
-docker run --detach --network=testnet                                         \
-  --name=vnc-server                                                           \
-  -p 5700:8080                                                                \
-  -e RUN_XTERM=no                                                             \
-  -e DISPLAY_WIDTH=1920                                                       \
-  -e DISPLAY_HEIGHT=1080                                                      \
-  -v "${ENV_ROOT}/temp/.X11-unix:/tmp/.X11-unix"                              \
-  theasp/novnc:latest
+  # try to change DISPLAY_WIDTH, DISPLAY_HEIGHT to make it fit in your screen,
+  # NOTE: please do not commit any change related to resolution.
+  docker run --detach --network=testnet                                                                     \
+    --name=vnc-server                                                                                       \
+    -p 5700:8080                                                                                            \
+    -e RUN_XTERM=no                                                                                         \
+    -e DISPLAY_WIDTH=1920                                                                                   \
+    -e DISPLAY_HEIGHT=1080                                                                                  \
+    -v "${ENV_ROOT}/temp/.X11-unix:/tmp/.X11-unix"                                                          \
+    theasp/novnc:latest
 
-###############
-### Cypress ###
-###############
+  ###############
+  ### Cypress ###
+  ###############
 
-# create cypress and attach its display to the VNC server container. 
-# this way you can view inside cypress container through vnc server.
-docker run --detach --network=testnet                                         \
-  --name="cypress.docker"                                                     \
-  -e DISPLAY=vnc-server:0.0                                                   \
-  -v "${ENV_ROOT}/docker/tls:/tls"                                            \
-  -v "${ENV_ROOT}/cypress/ocm-tests:/ocm"                                     \
-  -v "${ENV_ROOT}/temp/.X11-unix:/tmp/.X11-unix"                              \
-  -w /ocm                                                                     \
-  --entrypoint cypress                                                        \
-  cypress/included:13.3.0                                                     \
-  open --project .
+  # create cypress and attach its display to the VNC server container. 
+  # this way you can view inside cypress container through vnc server.
+  docker run --detach --network=testnet                                                                     \
+    --name="cypress.docker"                                                                                 \
+    -e DISPLAY=vnc-server:0.0                                                                               \
+    -v "${ENV_ROOT}/cypress/ocm-tests:/ocm"                                                                 \
+    -v "${ENV_ROOT}/temp/.X11-unix:/tmp/.X11-unix"                                                          \
+    -w /ocm                                                                                                 \
+    --entrypoint cypress                                                                                    \
+    cypress/included:13.3.0                                                                                 \
+    open --project .
 
-# print instructions.
-clear
-echo "Now browse to :"
-echo "Cypress inside VNC Server -> http://localhost:5700"
-echo "Embedded Firefox          -> http://localhost:5800"
-echo ""
-echo "Inside Embedded Firefox browse to EFSS hostname and enter the related credentials:"
-echo "https://owncloud1.docker  -> username: marie      password: radioactivity"
-echo "https://owncloud2.docker  -> username: mahdi      password: baghbani"
-echo "https://nextcloud1.docker -> username: einstein   password: relativity"
-echo "https://nextcloud2.docker -> username: michiel    password: dejong"
+  # print instructions.
+  clear
+  echo "Now browse to :"
+  echo "Cypress inside VNC Server -> http://localhost:5700"
+  echo "Embedded Firefox          -> http://localhost:5800"
+  echo ""
+  echo "Inside Embedded Firefox browse to EFSS hostname and enter the related credentials:"
+  echo "https://owncloud1.docker  -> username: marie      password: radioactivity"
+  echo "https://owncloud2.docker  -> username: mahdi      password: baghbani"
+  echo "https://nextcloud1.docker -> username: einstein   password: relativity"
+  echo "https://nextcloud2.docker -> username: michiel    password: dejong"
+else
+  ##################
+  ### Cypress CI ###
+  ##################
+
+  # run Cypress test suite headlessly and with the defined browser.
+  docker run --network=testnet                                                  \
+    --name="cypress.docker"                                                     \
+    -v "${ENV_ROOT}/cypress/ocm-tests:/ocm"                                     \
+    -w /ocm                                                                     \
+    cypress/included:13.3.0 cypress run --browser "${TEST_PLATFORM}"
+fi
