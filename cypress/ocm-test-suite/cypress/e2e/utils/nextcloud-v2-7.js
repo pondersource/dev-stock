7dfaadb19e84de6c5daa3b130732d26c10538166
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-03-21 07:50:32 +0000

    add: rename file and check for share existance

--- cypress/ocm-test-suite/cypress/e2e/utils/nextcloud-v2-7.js
+++ cypress/ocm-test-suite/cypress/e2e/utils/nextcloud-v2-7.js
@@ -1,7 +1,7 @@
 export function acceptShareV2_7() {
 	cy.get('div[class="oc-dialog"]', { timeout: 10000 })
       .should('be.visible')
-      .find('*[id^="oc-dialog-buttonrow"]')
+      .find('*[class^="oc-dialog-buttonrow"]')
       .find('button[class="primary"]')
       .click()
 }
@@ -23,17 +23,31 @@ export function createShareV2_7(fileName, username, domain) {
 	// TODO: check if it has been shared before with same user or not! (or reset share on both ends on each run for better developer experience, right now I have to manually clean and restart)
 }
 
+export function renameFileV2_7(fileName, newFileName) {
+	triggerActionInFileMenuV2_7(fileName, 'Rename')
+
+	// intercept the move so we can wait for it.
+	cy.intercept('MOVE', /\/remote.php\/dav\/files\//).as('moveFile')
+    getRowForFileV2_7(fileName).find('form').find('input').clear()
+	getRowForFileV2_7(fileName).find('form').find('input').type(`${newFileName}{enter}`)
+	cy.wait('@moveFile')
+}
+
 export function openSharingPanel(fileName) {
-	triggerActionForFile(fileName, 'Share')
+	triggerActionForFileV2_7(fileName, 'Share')
 
 	cy.get('#app-sidebar-vue')
 		.get('[aria-controls="tab-sharing"]')
 		.click()
 }
 
+export function triggerActionInFileMenuV2_7 (fileName, actionId) {
+	triggerActionForFileV2_7(fileName,'menu')
+	getRowForFileV2_7(fileName).find('*[class^="filename"]').find('*[class^="fileActionsMenu"]').find(`[data-action="${CSS.escape(actionId)}"]`).should('exist').click()
+}
 
-export const triggerActionForFile = (filename, actionId) => getActionsForFile(filename).find(`[data-action="${CSS.escape(actionId)}"]`).should('exist').click()
+export const triggerActionForFileV2_7 = (filename, actionId) => getActionsForFileV2_7(filename).find(`[data-action="${CSS.escape(actionId)}"]`).should('exist').click()
 
-export const getActionsForFile = (filename) => getRowForFile(filename).find('*[class^="filename"]').find('*[class^="name"]').find('*[class^="fileactions"]')
+export const getActionsForFileV2_7 = (filename) => getRowForFileV2_7(filename).find('*[class^="filename"]').find('*[class^="name"]').find('*[class^="fileactions"]')
 
-export const getRowForFile = (filename) => cy.get(`[data-file="${CSS.escape(filename)}"]`)
+export const getRowForFileV2_7 = (filename) => cy.get(`[data-file="${CSS.escape(filename)}"]`)
