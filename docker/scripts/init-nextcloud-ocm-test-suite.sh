ad69a427e731120cbc180900a2840b17949a5c33
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-03-21 06:05:29 +0000

    add: ocm test suite specific init script, disable sciencemesh

---
+++ docker/scripts/init-nextcloud-ocm-test-suite.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# @michielbdejong halt on error in docker init scripts
+set -e
+
+php console.php maintenance:install --admin-user "${USER}" --admin-pass "${PASS}" --database "mysql"            \
+                                    --database-name "efss" --database-user "root" --database-host "${DBHOST}"   \
+                                    --database-pass "eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek"
+php console.php app:disable firstrunwizard
+
+# change/add lines in config.php
+sed -i "3 i\  'allow_local_remote_servers' => true,"        /var/www/html/config/config.php
+sed -i "8 i\  1 => 'nc1.docker',"                           /var/www/html/config/config.php
+sed -i "9 i\  2 => 'nc2.docker',"                           /var/www/html/config/config.php
+sed -i "10 i\ 3 => 'nextcloud1.docker',"                    /var/www/html/config/config.php
+sed -i "11 i\ 4 => 'nextcloud2.docker',"                    /var/www/html/config/config.php
+
+# php console.php app:enable sciencemesh
