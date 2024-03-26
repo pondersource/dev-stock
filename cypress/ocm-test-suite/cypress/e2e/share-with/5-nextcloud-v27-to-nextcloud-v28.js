b3ea2d0e220c3aed40bdb4b87071eae6d2957076
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-03-26 18:14:38 +0000

    add: additional tests for v28

---
+++ cypress/ocm-test-suite/cypress/e2e/share-with/5-nextcloud-v27-to-nextcloud-v28.js
@@ -0,0 +1,35 @@
+import { createShareV27, renameFileV27 } from '../utils/nextcloud-v27'
+
+before(() => {
+  // makes custom commands available to all subsequent cy.origin('url')
+  // calls in this spec. put it in your support file to make them available to
+  // all specs
+  cy.origin('https://nextcloud3.docker', () => {
+    Cypress.require('../../support/commands')
+  })
+})
+
+describe('Native federated sharing functionality for Nextcloud v2.7', () => {
+  it('Accept federated share from Nextcloud to Nextcloud', () => {
+    // share from Nextcloud 1.
+    cy.loginNextcloud('https://nextcloud1.docker', 'einstein', 'relativity')
+
+    // renameFileV27('welcome.txt', 'nc1-to-nc2-share.txt')
+    createShareV27('nc1-to-nc2-share.txt', 'yashar', 'nextcloud3.docker')
+
+    // accept share from Nextcloud 3.
+    cy.origin('https://nextcloud3.docker', () => {
+      cy.loginNextcloud('/', 'yashar', 'pmh')
+
+      cy.get('div[class="oc-dialog"]', { timeout: 10000 })
+      .should('be.visible')
+      .find('*[class^="oc-dialog-buttonrow"]')
+      .find('button[class="primary"]')
+      .click()
+
+      // TODO: verify share received: 1. check for file name existence, 2. check if it can be downloaded, 3. compare checksum to the original file to make sure it is the same file.
+      // 1. check for filename existence.
+      cy.get('[data-file="nc1-to-nc2-share.txt"]', { timeout: 10000 }).should('be.visible')
+    })
+  })
+})
