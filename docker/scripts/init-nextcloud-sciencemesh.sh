669d86934a1815d6f625c0bdcdbb23fa2f6a6644
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-03-26 18:15:23 +0000

    add: new domains to trusted domains

--- docker/scripts/init-nextcloud-sciencemesh.sh
+++ docker/scripts/init-nextcloud-sciencemesh.sh
@@ -14,5 +14,7 @@ sed -i "8 i\  1 => 'nc1.docker',"                           /var/www/html/config
 sed -i "9 i\  2 => 'nc2.docker',"                           /var/www/html/config/config.php
 sed -i "10 i\ 3 => 'nextcloud1.docker',"                    /var/www/html/config/config.php
 sed -i "11 i\ 4 => 'nextcloud2.docker',"                    /var/www/html/config/config.php
+sed -i "12 i\ 5 => 'nextcloud3.docker',"                    /var/www/html/config/config.php
+sed -i "13 i\ 6 => 'nextcloud4.docker',"                    /var/www/html/config/config.php
 
 php console.php app:enable sciencemesh
