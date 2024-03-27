f39e2f79bf697e61ae16118385e8a15dfda69986
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-03-27 10:10:44 +0000

    modify: supress output

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -9,15 +9,15 @@ CLEAR_TERMINAL=${1:-"yes"}
 running=$(docker ps -q)
 # we actually need globbing and word spliting in this case.
 # shellcheck disable=SC2086
-[ -z "$running" ] || docker kill $running   >/dev/null 2>&1
+[ -z "$running" ] || docker kill $running                   >/dev/null 2>&1
 
 existing=$(docker ps -qa)
 # we actually need globbing and word spliting in this case.
 # shellcheck disable=SC2086
-[ -z "$existing" ] || docker rm $existing   >/dev/null 2>&1
+[ -z "$existing" ] || docker rm $existing                   >/dev/null 2>&1
 
-docker network remove testnet || true       >/dev/null 2>&1
-docker network create testnet               >/dev/null 2>&1
+docker network remove testnet >/dev/null 2>&1 || true       >/dev/null 2>&1
+docker network create testnet                               >/dev/null 2>&1
 
 # I want a clean terminal xD
 if [ "${CLEAR_TERMINAL}" = "yes" ]; then
