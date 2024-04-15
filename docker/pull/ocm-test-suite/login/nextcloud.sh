f4f08a0b0b28123ef4e697711b37596ddc09d5cd
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-04-15 10:47:52 +0000

    add: optimize docker pulls even more

--- docker/pull/ocm-test-suite/login/nextcloud.sh
+++ docker/pull/ocm-test-suite/login/nextcloud.sh
@@ -3,10 +3,11 @@
 # @michielbdejong halt on error in docker init scripts.
 set -e
 
+EFSS_PLATFORM_VERSION=${1:-"v27.1.7"}
+
 # 3rd party images.
 docker pull mariadb:latest
 docker pull cypress/included:13.3.0
 
 # dev-stock images.
-docker pull pondersource/dev-stock-nextcloud:v28.0.3
-docker pull pondersource/dev-stock-nextcloud:v27.1.7
+docker pull "pondersource/dev-stock-nextcloud:${EFSS_PLATFORM_VERSION}"
