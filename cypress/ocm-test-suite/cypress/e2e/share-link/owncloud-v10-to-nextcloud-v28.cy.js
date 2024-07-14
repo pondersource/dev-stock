d0921613f04925f476913c316c702c4c2fb99523
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-07-14 09:11:18 +0000

    add: oc to nc v28 sharelink test

---
+++ cypress/ocm-test-suite/cypress/e2e/share-link/owncloud-v10-to-nextcloud-v28.cy.js
@@ -0,0 +1,49 @@
+import { createShareLink, renameFile } from '../utils/owncloud'
+
+describe('Share link federated sharing functionality for ownCloud', () => {
+  it('Send federated share <file> from ownCloud v10 to Nextcloud v28', () => {
+    // share from ownCloud 1.
+    cy.loginOwncloud('https://owncloud1.docker', 'marie', 'radioactivity')
+
+    renameFile('welcome.txt', 'oc1-to-nc1-share-link.txt')
+    createShareLink('oc1-to-nc1-share-link.txt').then(
+      (result) => {
+        cy.visit(result)
+
+        // save share url to file.
+        cy.writeFile('share-link-url.txt', result)
+      }
+    )
+  })
+
+  it('Receive federated share <file> from ownCloud v10 to Nextcloud v28', () => {
+
+    // load share url from file.
+    cy.readFile('share-link-url.txt').then((result) => {
+      
+      // extract token from url.
+      const token = result.replace('https://owncloud1.docker/index.php/s/','');
+
+      // put token into the link.
+      const url = `https://nextcloud1.docker/index.php/login?redirect_url=%252Findex.php%252Fapps%252Ffiles#remote=https%3A%2F%2Fowncloud1.docker&token=${token}&owner=marie&ownerDisplayName=marie&name=oc1-to-oc2-share-link.txt&protected=0`
+      
+      // accept share from Nextcloud 1.
+      cy.loginNextcloudCore('https://nextcloud1.docker', 'einstein', 'relativity')
+
+      cy.visit(url)
+
+      cy.get('div[class="oc-dialog"]', { timeout: 10000 })
+        .should('be.visible')
+        .find('*[class^="oc-dialog-buttonrow"]')
+        .find('button[class="primary"]')
+        .click()
+
+      // force reload the page for share to apear.
+      cy.reload(true)
+
+      // 1. check for filename existence.
+      cy.get('[data-cy-files-list-row-name="oc1-to-nc1-share-link.txt"]', { timeout: 10000 }).should('be.visible')
+    
+    })
+  })
+})
