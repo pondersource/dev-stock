e486d405323194a8a9b37d1967e671ea3ced91bd
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-07-14 12:19:28 +0000

    add: nc 28 to oc 10 share link test

---
+++ cypress/ocm-test-suite/cypress/e2e/share-link/nextcloud-v28-to-owncloud-v10.cy.js
@@ -0,0 +1,37 @@
+import { createShareLinkV28, renameFileV28 } from '../utils/nextcloud-v28'
+
+describe('Share link federated sharing functionality for Nextcloud', () => {
+  it('Send federated share <file> from Nextcloud v28 to ownCloud v10', () => {
+    // send share from Nextcloud 1.
+    cy.loginNextcloud('https://nextcloud1.docker', 'einstein', 'relativity')
+
+    renameFileV28('welcome.txt', 'nc1-to-oc1-share-link.txt')
+    createShareLinkV28('nc1-to-oc1-share-link.txt').then(
+      (result) => {
+        cy.visit(result)
+
+        cy.get('button[id="header-actions-toggle"]').click()
+        cy.get('button[id="save-external-share"]').click()
+        
+        cy.get('form[class="save-form"]').within(() => {
+          cy.get('input[id="remote_address"]').type('marie@owncloud1.docker')
+          cy.get('input[id="save-button-confirm"]').click()
+        })
+      }
+    )
+  })
+
+  it('Receive federated share <file> from Nextcloud v28 to ownCloud v10', () => {
+    // accept share from Nextcloud 1.
+    cy.loginOwncloud('https://owncloud1.docker', 'marie', 'radioactivity')
+
+    cy.get('div[class="oc-dialog"]', { timeout: 10000 })
+      .should('be.visible')
+      .find('*[class^="oc-dialog-buttonrow"]')
+      .find('button[class="primary"]')
+      .click()
+
+    // 1. check for filename existence.
+    cy.get('[data-file="nc1-to-oc1-share-link.txt"]', { timeout: 10000 }).should('be.visible')
+  })
+})
