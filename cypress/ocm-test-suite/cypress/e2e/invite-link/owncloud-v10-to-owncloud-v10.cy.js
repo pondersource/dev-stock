cbe81883e3bd2ca4728c94ecc66c9f1cc457b962
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-07-22 17:27:48 +0000

    add: owncloud to owncloud invite link share

---
+++ cypress/ocm-test-suite/cypress/e2e/invite-link/owncloud-v10-to-owncloud-v10.cy.js
@@ -0,0 +1,58 @@
+import { createInviteLink, createSienceMeshShare, renameFile } from '../utils/owncloud'
+
+describe('invite link federated sharing via ScienceMesh functionality for ownCloud', () => {
+  it('Send invitation from ownCloud v10 to ownCloud v10', () => {
+    // share from ownCloud 1.
+    cy.loginOwncloud('https://owncloud1.docker', 'marie', 'radioactivity')
+    cy.visit('https://owncloud1.docker/index.php/apps/sciencemesh/')
+
+
+    createInviteLink('https://owncloud2.docker').then(
+      (result) => {
+        // save invite link to file.
+        cy.writeFile('invite-link-oc-oc.txt', result)
+      }
+    )
+  })
+
+  it('Accept invitation from ownCloud v10 to ownCloud v10', () => {
+
+    // load share url from file.
+    cy.readFile('invite-link-oc-oc.txt').then((url) => {
+      
+      // accept share from ownCloud 2.
+      cy.loginOwncloudCore(url, 'mahdi', 'baghbani')
+
+      cy.get('input[id="accept-button"]', { timeout: 10000 })
+        .click()
+      
+      // validate 'marie' is shown as a contact. 
+      cy.visit('https://owncloud2.docker/index.php/apps/sciencemesh/contacts')
+
+      cy.get('table[id="contact-table"]')
+        .find('p[class="displayname"]')
+        .should("have.text", "marie");
+    })
+  })
+
+  it('Send ScienceMesh share <file> from ownCloud v10 to ownCloud v10', () => {
+    // share from ownCloud 1.
+    cy.loginOwncloud('https://owncloud1.docker', 'marie', 'radioactivity')
+
+    renameFile('welcome.txt', 'oc1-to-oc2-sciencemesh-share.txt')
+    createSienceMeshShare('oc1-to-oc2-sciencemesh-share.txt', 'mahdi', 'owncloud2.docker')
+  })
+
+  it('Receive ScienceMesh share <file> from ownCloud v10 to ownCloud v10', () => {
+    // accept share from Nextcloud 2.
+    cy.loginOwncloud('https://owncloud2.docker', 'mahdi', 'baghbani')
+
+    cy.get('div[class="oc-dialog"]', { timeout: 10000 })
+      .should('be.visible')
+      .find('*[class^="oc-dialog-buttonrow"]')
+      .find('button[class="primary"]')
+      .click()
+
+    cy.get('[data-file="oc1-to-oc2-sciencemesh-share.txt"]', { timeout: 10000 }).should('be.visible')
+  })
+})
