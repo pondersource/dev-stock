9e47a56d8ee92a063b55a563928d89e1cb685905
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-07-27 11:23:15 +0000

    fix: typo

--- cypress/ocm-test-suite/cypress/e2e/invite-link/ocis-5-to-owncloud-v10.cy.js
+++ cypress/ocm-test-suite/cypress/e2e/invite-link/ocis-5-to-owncloud-v10.cy.js
@@ -32,7 +32,7 @@ describe('Invite link federated sharing via ScienceMesh functionality for oCIS',
       cy.get('input[id="accept-button"]', { timeout: 10000 })
         .click()
 
-      // validate 'marie' is shown as a contact. 
+      // validate 'Albert Einstein' is shown as a contact. 
       cy.visit('https://owncloud1.docker/index.php/apps/sciencemesh/contacts')
 
       cy.get('table[id="contact-table"]')
