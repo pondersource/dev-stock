ee0b0425682f9b8f40342f29a6dc18a99966d09f
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-07-27 08:01:58 +0000

    modify: trun steps into reusable functions

--- cypress/ocm-test-suite/cypress/e2e/invite-link/ocis-5-to-ocis-5.cy.js
+++ cypress/ocm-test-suite/cypress/e2e/invite-link/ocis-5-to-ocis-5.cy.js
@@ -1,4 +1,14 @@
-import { openFilesAppV5, openScienceMeshAppV5, createInviteLinkV5, createShareV5 } from '../utils/ocis-5'
+import {
+  openFilesAppV5,
+  openScienceMeshAppV5,
+  createInviteLinkV5,
+  acceptInviteLinkV5,
+  verifyFederatedContactV5,
+  createTextFileV5,
+  createShareV5,
+  acceptShareV5,
+  verifyShareV5
+} from '../utils/ocis-5'
 
 describe('Invite link federated sharing via ScienceMesh functionality for oCIS', () => {
   it('Send invitation from oCIS v5 to oCIS v5', () => {
@@ -19,64 +29,12 @@ describe('Invite link federated sharing via ScienceMesh functionality for oCIS',
 
     // load invite token from file.
     cy.readFile('invite-link-ocis-ocis.txt').then((token) => {
-      
+
       cy.loginOcis('https://ocis2.docker', 'marie', 'radioactivity')
 
-      openScienceMeshAppV5()
+      acceptInviteLinkV5(token)
 
-      cy.get('div[id="sciencemesh-accept-invites"]')
-        .find('label')
-        .contains('Enter invite token')
-        .parent()
-        .scrollIntoView()
-        .should('be.visible')
-        .within(() => {
-          cy.get('input[type="text"]')
-          .type(token)
-        })
-
-      cy.get('div[id="sciencemesh-accept-invites"]')
-        .find('label')
-        .contains('Select institution of inviter')
-        .parent()
-        .scrollIntoView()
-        .should('be.visible')
-        .find('div[class="vs__actions"')
-        .click()
-
-      cy.get('div[id="sciencemesh-accept-invites"]')
-        .find('label')
-        .contains('Select institution of inviter')
-        .parent()
-        .find('ul[role="listbox"]')
-        .find('li')
-        .first()
-        .click()
-
-      cy.get('div[id="sciencemesh-accept-invites"]')
-        .find('span')
-        .contains('Accept invitation')
-        .parent()
-        .scrollIntoView()
-        .should('be.visible')
-        .click()
-      
-      // check if federation connection is added.
-      cy.get('div[id="sciencemesh-connections"]')
-        .get('table')
-        .find('tbody>tr')
-        .eq(0)
-        .find('td')
-        .eq(0)
-        .should("have.text", "Albert Einstein")
-
-      cy.get('div[id="sciencemesh-connections"]')
-        .get('table')
-        .find('tbody>tr')
-        .eq(0)
-        .find('td')
-        .eq(2)
-        .should("have.text", "ocis1.docker")
+      verifyFederatedContactV5('Albert Einstein', 'ocis1.docker')
     })
   })
 
@@ -84,62 +42,25 @@ describe('Invite link federated sharing via ScienceMesh functionality for oCIS',
     // share from oCIS 1.
     cy.loginOcis('https://ocis1.docker', 'einstein', 'relativity')
 
-    cy.get('button[id="new-file-menu-btn"]')
-      .scrollIntoView()
-      .should('be.visible')
-      .click()
-    
-    cy.get('div[id="new-file-menu-drop"]')
-      .scrollIntoView()
-      .should('be.visible')
-      .find('ul[id="create-list"]')
-      .find('span')
-      .contains('txt')
-      .parent()
-      .click()
-
-    cy.get('div[class="oc-modal-background"]')
-      .scrollIntoView()
-      .should('be.visible')
-      .within(() => {
-        cy.get('input[id="oc-textinput-10"]').clear()
-        cy.get('input[id="oc-textinput-10"]').type('invite-link-ocis-ocis.txt')
-
-        cy.get('button')
-          .contains('Create')
-          .scrollIntoView()
-          .should('be.visible')
-          .click()
-      })
-    
-    cy.get('textarea[id="text-editor-input"]')
-      .scrollIntoView()
-      .should('be.visible')
-      .type('Hello World!')
-
-    cy.get('button[id="app-save-action"]')
-      .scrollIntoView()
-      .should('be.visible')
-      .click()
+    createTextFileV5('invite-link-ocis-ocis.txt', 'Hello World!')
 
     openFilesAppV5()
 
     createShareV5('invite-link-ocis-ocis.txt', 'marie')
-
   })
 
   it('Receive ScienceMesh share <file> from oCIS v5 to oCIS v5', () => {
     // accept share from oCIS 2.
     cy.loginOcis('https://ocis2.docker', 'marie', 'radioactivity')
 
-    cy.get('div[id="web-nav-sidebar"]')
-      .should('be.visible')
-      .find('span')
-      .contains('Shares')
-      .click()
+    acceptShareV5('invite-link-ocis-ocis.txt')
 
-    cy.get('span[data-test-resource-name="invite-link-ocis-ocis.txt"]')
-      .scrollIntoView()
-      .should('be.visible')
+    cy.reload(true)
+
+    verifyShareV5(
+      'invite-link-ocis-ocis.txt',
+      'Albert Einstein',
+      'Marie Skłodowska Curie'
+    )
   })
 })
