b2ec282ccceba59207d02ccc8826ff829cf76192
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-07-28 09:45:45 +0000

    add: new functions to verify fed contact and create sicencemesh share

--- cypress/ocm-test-suite/cypress/e2e/utils/nextcloud-v27.js
+++ cypress/ocm-test-suite/cypress/e2e/utils/nextcloud-v27.js
@@ -1,9 +1,9 @@
 export function acceptShareV27() {
 	cy.get('div[class="oc-dialog"]', { timeout: 10000 })
-      .should('be.visible')
-      .find('*[class^="oc-dialog-buttonrow"]')
-      .find('button[class="primary"]')
-      .click()
+		.should('be.visible')
+		.find('*[class^="oc-dialog-buttonrow"]')
+		.find('button[class="primary"]')
+		.click()
 }
 
 export function createShareV27(fileName, username, domain) {
@@ -25,16 +25,16 @@ export function createShareLinkV27(fileName) {
 	openSharingPanelV27(fileName)
 
 	return cy.window().then(win => {
-        cy.stub(win.navigator.clipboard, 'writeText').as('copy');
+		cy.stub(win.navigator.clipboard, 'writeText').as('copy');
 
-        cy.get('#app-sidebar-vue').within(() => {
+		cy.get('#app-sidebar-vue').within(() => {
 			cy.get('*[class^="sharing-entry__actions"]').eq(0).click()
 		})
 
-        return cy.get('@copy').should('have.been.calledOnce').then((spy) => {
-            return (spy).lastCall.args[0];
-        });
-    })
+		return cy.get('@copy').should('have.been.calledOnce').then((spy) => {
+			return (spy).lastCall.args[0];
+		});
+	})
 }
 
 export function createInviteLinkV27(targetDomain) {
@@ -46,14 +46,39 @@ export function createInviteLinkV27(targetDomain) {
 		.then(
 			sometext => {
 				// extract token from url.
-				const token = sometext.replace('https://meshdir.docker/meshdir?','');
+				const token = sometext.replace('https://meshdir.docker/meshdir?', '');
 
 				// put target efss domain and token together.
 				const inviteLink = `${targetDomain}/index.php/apps/sciencemesh/accept?${token}`
-      
+
 				return inviteLink
 			}
 		);
+}
+
+export function verifyFederatedContactV27(domain, displayName, contactDomain) {
+	cy.visit(`https://${domain}/index.php/apps/sciencemesh/contacts`)
+
+	cy.get('table[id="contact-table"]')
+		.find('p[class="displayname"]')
+		.should("have.text", displayName)
+
+	cy.get('table[id="contact-table"]')
+		.find('p[class="displayname"]')
+		.contains(displayName)
+		.parent()
+		.parent()
+		.find('p[class="username-provider"]')
+		.invoke('text')
+		.then(
+			usernameWithDomain => {
+				const extractedDomain = usernameWithDomain.substring(
+					usernameWithDomain.lastIndexOf("@") + 1, usernameWithDomain.length
+				)
+
+				expect(extractedDomain).equal(contactDomain);
+			}
+		)
 }
 
 export function createScienceMeshShareV27(fileName, username) {
@@ -70,13 +95,49 @@ export function createScienceMeshShareV27(fileName, username) {
 	cy.get('div[class="button-group"]').contains('Save share').click()
 }
 
+export function getScienceMeshContactIdFromDisplayNameV27(domain, displayName, contactDomain) {
+	verifyFederatedContactV27(domain, displayName, contactDomain)
+
+	return cy.get('table[id="contact-table"]')
+		.find('p[class="displayname"]')
+		.contains(displayName)
+		.parent()
+		.parent()
+		.find('p[class="username-provider"]')
+		.invoke('text')
+		.then(
+			usernameWithDomain => {
+				return usernameWithDomain
+			}
+		)
+}
+
+export function createScienceMeshShareAdvancedV27(domain, displayName, contactDomain, filename) {
+	getScienceMeshContactIdFromDisplayNameV27(domain, displayName, contactDomain).then(
+		(shareWith) => {
+			cy.visit(`https://${domain}/index.php/apps/files`)
+
+			openSharingPanelV27(filename)
+
+			cy.get('#app-sidebar-vue').within(() => {
+				cy.get('#sharing-search-input').clear()
+				cy.intercept({ times: 1, method: 'GET', url: '**/apps/files_sharing/api/v1/sharees?*' }).as('userSearch')
+				cy.get('#sharing-search-input').type(displayName)
+				cy.wait('@userSearch')
+			})
+
+			cy.get(`[sharewith="${shareWith}"]`).eq(0).click()
+			cy.get('div[class="button-group"]').contains('Save share').click()
+		}
+	)
+}
 
 export function renameFileV27(fileName, newFileName) {
 	triggerActionInFileMenuV27(fileName, 'Rename')
 
 	// intercept the move so we can wait for it.
 	cy.intercept('MOVE', /\/remote.php\/dav\/files\//).as('moveFile')
-    getRowForFileV27(fileName).find('form').find('input').clear()
+	getRowForFileV27(fileName).find('form').find('input').clear()
 	getRowForFileV27(fileName).find('form').find('input').type(`${newFileName}{enter}`)
 	cy.wait('@moveFile')
 }
@@ -89,8 +150,8 @@ export function openSharingPanelV27(fileName) {
 		.click()
 }
 
-export function triggerActionInFileMenuV27 (fileName, actionId) {
-	triggerActionForFileV27(fileName,'menu')
+export function triggerActionInFileMenuV27(fileName, actionId) {
+	triggerActionForFileV27(fileName, 'menu')
 	getRowForFileV27(fileName).find('*[class^="filename"]').find('*[class^="fileActionsMenu"]').find(`[data-action="${CSS.escape(actionId)}"]`).should('exist').click()
 }
 
