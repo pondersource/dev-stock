d006d725dfa38ad7c74dcd8b1edffbdb1abca876
Michiel de Jong <michiel@pondersource.com>
2024-08-28 15:18:10 +0200

    simplification

--- dev/ocm-test-suite/login/ocmstub.sh
+++ dev/ocm-test-suite/login/ocmstub.sh
@@ -71,17 +71,9 @@ function createEfss() {
     --name="${platform}${number}.docker"                                                                                    \
     --add-host "host.docker.internal:host-gateway"                                                                          \
     -e HOST="${platform}${number}"                                                                                          \
-    -e DBHOST="maria${platform}${number}.docker"                                                                            \
-    -e USER="${user}"                                                                                                       \
-    -e PASS="${password}"                                                                                                   \
-    -v "${ENV_ROOT}/docker/tls/certificates:/certificates"                                                                  \
-    -v "${ENV_ROOT}/docker/tls/certificate-authority:/certificate-authority"                                                \
-    -v "${ENV_ROOT}/temp/${init_script}:/${platform}-init.sh"                                                               \
-    -v "${ENV_ROOT}/docker/scripts/entrypoint.sh:/entrypoint.sh"                                                            \
     "${image}:${tag}"
 
   # wait for hostname port to be open.
-  waitForPort "maria${platform}${number}.docker"  3306
   waitForPort "${platform}${number}.docker"       443
 
   # add self-signed certificates to os and trust them. (use >/dev/null 2>&1 to shut these up)
@@ -90,9 +82,6 @@ function createEfss() {
   docker exec "${platform}${number}.docker" bash -c "cp -f /tls/*.crt                             /usr/local/share/ca-certificates/ || true"            >/dev/null 2>&1
   docker exec "${platform}${number}.docker" update-ca-certificates                                                                                      >/dev/null 2>&1
   docker exec "${platform}${number}.docker" bash -c "cat /etc/ssl/certs/ca-certificates.crt >> /var/www/html/resources/config/ca-bundle.crt"            >/dev/null 2>&1
-
-  # run init script inside efss.
-  redirect_to_null_cmd docker exec -u www-data "${platform}${number}.docker" bash "/${platform}-init.sh"
 
   redirect_to_null_cmd echo ""
 }
