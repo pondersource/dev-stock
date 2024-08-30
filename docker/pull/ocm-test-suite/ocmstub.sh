3f3403a3482c7378cc1f854171e3bf819331dc61
Michiel de Jong <michiel@pondersource.com>
2024-08-30 13:34:51 +0200

    Pull ocmstub image

---
+++ docker/pull/ocm-test-suite/ocmstub.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# @michielbdejong halt on error in docker init scripts.
+set -e
+
+# nextcloud version:
+#   - v27.1.10
+#   - v28.0.7
+EFSS_PLATFORM_VERSION=${1:-"1.0"}
+
+# dev-stock images.
+docker pull "pondersource/dev-stock-ocmstub:${EFSS_PLATFORM_VERSION}"
--- docker/push/ocm-test-suite.sh
+++ docker/push/ocm-test-suite.sh
@@ -8,4 +8,5 @@ docker login
 docker push pondersource/dev-stock-revad:latest
 docker push pondersource/dev-stock-nextcloud-sciencemesh:latest
 docker push pondersource/dev-stock-owncloud-sciencemesh:latest
-docker push pondersource/dev-stock-owncloud-ocm-test-suite:latest:latest
+docker push pondersource/dev-stock-owncloud-ocm-test-suite:latest
+docker push pondersource/dev-stock-ocmstub:1.0
