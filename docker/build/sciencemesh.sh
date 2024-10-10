ed80642ab261386692e27c18227a55642c9f69f2
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2024-10-10 10:46:32 +0000

    add: nextcloud version 30

--- docker/build/sciencemesh.sh
+++ docker/build/sciencemesh.sh
@@ -30,7 +30,10 @@ echo Building pondersource/dev-stock-php-base
 docker build --build-arg CACHEBUST="default" --file ./dockerfiles/php-base.Dockerfile --tag pondersource/dev-stock-php-base:latest .
 
 echo Building pondersource/dev-stock-nextcloud
-docker build --build-arg CACHEBUST="default" --build-arg BRANCH_NEXTCLOUD="v28.0.7" --file ./dockerfiles/nextcloud.Dockerfile --tag pondersource/dev-stock-nextcloud:v28.0.7 --tag pondersource/dev-stock-nextcloud:latest .
+docker build --build-arg CACHEBUST="default" --build-arg BRANCH_NEXTCLOUD="v30.0.0" --file ./dockerfiles/nextcloud.Dockerfile --tag pondersource/dev-stock-nextcloud:v30.0.0 --tag pondersource/dev-stock-nextcloud:latest .
+
+echo Building pondersource/dev-stock-nextcloud
+docker build --build-arg CACHEBUST="default" --build-arg BRANCH_NEXTCLOUD="v28.0.7" --file ./dockerfiles/nextcloud.Dockerfile --tag pondersource/dev-stock-nextcloud:v28.0.7
 
 echo Building pondersource/dev-stock-nextcloud
 docker build --build-arg CACHEBUST="default" --build-arg BRANCH_NEXTCLOUD="v27.1.10" --file ./dockerfiles/nextcloud.Dockerfile --tag pondersource/dev-stock-nextcloud:v27.1.10 .
--- docker/pull/sciencemesh.sh
+++ docker/pull/sciencemesh.sh
@@ -6,5 +6,6 @@ set -e
 docker pull pondersource/dev-stock-revad:latest
 docker pull pondersource/dev-stock-nextcloud:v28.0.7
 docker pull pondersource/dev-stock-nextcloud:v27.1.10
+docker pull pondersource/dev-stock-nextcloud:v30.0.0
 docker pull pondersource/dev-stock-nextcloud-sciencemesh:latest
 docker pull pondersource/dev-stock-owncloud-sciencemesh:latest
