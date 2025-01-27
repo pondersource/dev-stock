1f7dee94bbb1338ecd62cf708b1e417186c39ae8
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2025-01-27 11:32:42 +0330

    fix: typo

--- docker/scripts/init/nc-sm.sh
+++ docker/scripts/init/nc-sm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ln -sf /ponder/apps/sciencemesh /var/html/www/apps/sciencemesh
+ln -sf /ponder/apps/sciencemesh /var/www/html/apps/sciencemesh
 
 php console.php app:enable --force sciencemesh
