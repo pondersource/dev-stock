baa92dac31a907af168d91e5dc33c5b5ca43d857
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2025-05-14 09:41:34 +0000

    fix: importing file that doesn't exist

--- cypress/ocm-test-suite/cypress/support/e2e.js
+++ cypress/ocm-test-suite/cypress/support/e2e.js
@@ -1,6 +1,3 @@
-// Import commands.js using ES2015 syntax:
-import './commands'
-
 const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/
 Cypress.on('uncaught:exception', (err) => {
     /* returning false here prevents Cypress from failing the test */
